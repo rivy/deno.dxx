0b923c2dace2c32fd4abdc233c7413d3a42edd17
Roy Ivy III <rivy.dev@gmail.com>
2021-05-25 18:25:50 -0500

    refactor shim detection and enhancement code into a shared library

---
+++ src/lib/shim.windows.ts
@@ -0,0 +1,83 @@
+// spell-checker:ignore (vars) ARGX
+
+const cmdShimBase = `% \`<%=shimBinName%>\` (*enhanced* Deno CMD shim; by \`dxi\`) %
+@rem:: spell-checker:ignore (shell/CMD) COMSPEC ERRORLEVEL ; (deno) hrtime ; (bin) <%=shimBinName%> <%=denoRunTarget%>
+@set "ERRORLEVEL="
+@set "DENO_SHIM_ERRORLEVEL="
+@setLocal
+@set "DENO_SHIM_PIPE="
+@set "DENO_SHIM_ARGS="
+@set "DENO_SHIM_ARGX="
+@set "DENO_SHIM_URL="
+@:...prep...
+@:launch
+@rem:: DENO_SHIM_EXEC convolution is to avoid \`%*\` within the final parse group [o/w paren args cause parsing misbehavior]
+@>>"%DENO_SHIM_EXEC%" echo @set DENO_SHIM_ARGS=%*
+@>>"%DENO_SHIM_EXEC%" echo @goto _undef_ 2^>NUL ^|^| @for %%%%G in ("%COMSPEC%") do @title %%%%~nG ^& @deno.exe "run" <%= denoRunOptions ? (denoRunOptions + ' ') : '' %>-- <%=denoRunTarget%> %%DENO_SHIM_ARGS%%
+@(
+@goto _undef_ 2>NUL
+@for %%G in ("%COMSPEC%") do @title %%~nG
+@set "DENO_SHIM_EXEC=%DENO_SHIM_EXEC%"
+@set "DENO_SHIM_PIPE=%DENO_SHIM_PIPE%"
+@set DENO_SHIM_ARG0=%~0
+@set DENO_SHIM_URL=<%=denoRunTarget%>
+@call "%DENO_SHIM_EXEC%"
+@call set DENO_SHIM_ERRORLEVEL=%%ERRORLEVEL%%
+@if EXIST "%DENO_SHIM_PIPE%" call "%DENO_SHIM_PIPE%" >NUL 2>NUL
+@if EXIST "%DENO_SHIM_EXEC%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_EXEC%" 2>NUL
+@if EXIST "%DENO_SHIM_PIPE%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_PIPE%" 2>NUL
+@set "DENO_SHIM_EXEC="
+@set "DENO_SHIM_PIPE="
+@set "DENO_SHIM_ARG0="
+@set "DENO_SHIM_ARGS="
+@set "DENO_SHIM_ARGX="
+@set "DENO_SHIM_URL="
+@call %COMSPEC% /d/c "exit %%DENO_SHIM_ERRORLEVEL%%"
+)
+`;
+const cmdShimPrepPipe = `@:pipeEnabled
+@:prep
+@set "RANDOM=" &:: remove any cloak from dynamic variable RANDOM
+@if NOT DEFINED TEMP @set TEMP=%TMP%
+@if NOT EXIST "%TEMP%" @set TEMP=%TMP%
+@if NOT EXIST "%TEMP%" @goto :launch
+@set DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%
+@set DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd
+@set DENO_SHIM_PIPE=%TEMP%\\<%=shimBinName%>.shim.pipe.%DENO_SHIM_TID%.cmd
+@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
+@if EXIST "%DENO_SHIM_PIPE%" @goto :prep
+@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"
+@if DEFINED DENO_SHIM_PIPE echo @rem \`<%=shimBinName%>\` shell pipe > "%DENO_SHIM_PIPE%"`;
+const cmdShimPrepNoPipe = `@:pipeDisabled
+@:prep
+@set "RANDOM=" &:: remove any cloak from dynamic variable RANDOM
+@if NOT DEFINED TEMP @set TEMP=%TMP%
+@if NOT EXIST "%TEMP%" @set TEMP=%TMP%
+@if NOT EXIST "%TEMP%" @goto :launch
+@set DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%
+@set DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd
+@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
+@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"`;
+
+export function cmdShimTemplate(enablePipe: boolean) {
+	return cmdShimBase.replace('@:...prep...', enablePipe ? cmdShimPrepPipe : cmdShimPrepNoPipe);
+}
+
+export function shimInfo(contentsOriginal: string) {
+	// heuristic match for enhanced shim
+	// spell-checker:ignore () ined
+	const isEnhanced = contentsOriginal.match(/goto\s+[\W_]*undef(?:ined)?[\W_]*\s+2\s*>\s*NUL/i) ||
+		contentsOriginal.match(/shim\s*;\s*by\s*`?dxi`?/i);
+
+	const reMatchArray = contentsOriginal.match(
+		// eg, `@deno run "--allow-..." ... "https://deno.land/x/denon/denon.ts" %*`
+		/^(.*?)@\x22?deno(?:[.]exe)?\x22?\s+\x22?run\x22?\s+(.*\s+)?(\x22[^\x22]*\x22)\s+%*.*$/m,
+	) || [];
+	const [_match, _denoCommandPrefix, denoRunOptionsRaw, denoRunTarget] = reMatchArray;
+
+	const denoRunOptions = (denoRunOptionsRaw || '')
+		.replace(/((^|\s+)\x22?--\x22?)+(\s|$)/g, ' ') // remove any "--" (quoted or not); avoids collision with "--" added by template
+		.replace(/^\s+/m, '') // remove leading whitespace
+		.replace(/\s+$/m, ''); // remove trailing whitespace
+	return { isEnhanced, denoRunOptions, denoRunTarget };
+}
--- tools/enhance-deno-CMD-shims.deno.ts
+++ tools/enhance-deno-CMD-shims.deno.ts
@@ -22,6 +22,9 @@ const fs = { exists, existsSync, expandGlob, expandGlobSync, walk, walkSync };
 
 import { collect, filter, map } from './lib/funk.ts';
 
+const enablePipe = true;
+const forceUpdate = true;
+
 // templating engines ~ <https://colorlib.com/wp/top-templating-engines-for-javascript> @@ <https://archive.is/BKYMw>
 
 // lodash
@@ -39,73 +42,6 @@ import * as _ from 'https://cdn.skypack.dev/pin/lodash@v4.17.20-4NISnx5Etf8JOo22
 
 const decoder = new TextDecoder(); // default == 'utf-8'
 const encoder = new TextEncoder(); // default == 'utf-8'
-
-const cmdShimBase = `% \`<%=shimBinName%>\` (*enhanced* Deno CMD shim; by \`dxi\`) %
-@rem:: spell-checker:ignore (shell/CMD) COMSPEC ERRORLEVEL ; (deno) Deno hrtime ; (bin) <%=shimBinName%> <%=denoRunTarget%>
-@set "ERRORLEVEL="
-@set "DENO_SHIM_ERRORLEVEL="
-@setLocal
-@set "DENO_SHIM_PIPE="
-@set "DENO_SHIM_ARGS="
-@set "DENO_SHIM_ARGx="
-@set "DENO_SHIM_URL="
-@:...prep...
-@:launch
-@rem:: DENO_SHIM_EXEC convolution is to avoid \`%*\` within the final parse group [o/w args with parentheses cause parsing misbehavior]
-@>>"%DENO_SHIM_EXEC%" echo @set DENO_SHIM_ARGS=%*
-@>>"%DENO_SHIM_EXEC%" echo @goto _undef_ 2^>NUL ^|^| @for %%%%G in ("%COMSPEC%") do @title %%%%~nG ^& @deno.exe "run" <%= denoRunOptions ? (denoRunOptions + ' ') : '' %>-- <%=denoRunTarget%> %%DENO_SHIM_ARGS%%
-@(
-@goto _undef_ 2>NUL
-@for %%G in ("%COMSPEC%") do @title %%~nG
-@set "DENO_SHIM_EXEC=%DENO_SHIM_EXEC%"
-@set "DENO_SHIM_PIPE=%DENO_SHIM_PIPE%"
-@set DENO_SHIM_ARG0=%~0
-@set DENO_SHIM_URL=<%=denoRunTarget%>
-@call "%DENO_SHIM_EXEC%"
-@call set DENO_SHIM_ERRORLEVEL=%%ERRORLEVEL%%
-@if EXIST "%DENO_SHIM_PIPE%" call "%DENO_SHIM_PIPE%" >NUL 2>NUL
-@if EXIST "%DENO_SHIM_EXEC%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_EXEC%" 2>NUL
-@if EXIST "%DENO_SHIM_PIPE%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_PIPE%" 2>NUL
-@set "DENO_SHIM_EXEC="
-@set "DENO_SHIM_PIPE="
-@set "DENO_SHIM_ARG0="
-@set "DENO_SHIM_ARGS="
-@set "DENO_SHIM_ARGx="
-@set "DENO_SHIM_URL="
-@call %COMSPEC% /d/c "exit %%DENO_SHIM_ERRORLEVEL%%"
-)
-`;
-const cmdShimPrepPipe = `@:pipeEnabled
-@:prep
-@set "RANDOM=" &:: remove any cloak from dynamic variable RANDOM
-@if NOT DEFINED TEMP @set TEMP=%TMP%
-@if NOT EXIST "%TEMP%" @set TEMP=%TMP%
-@if NOT EXIST "%TEMP%" @goto :launch
-@set DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%
-@set DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd
-@set DENO_SHIM_PIPE=%TEMP%\\<%=shimBinName%>.shim.pipe.%DENO_SHIM_TID%.cmd
-@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
-@if EXIST "%DENO_SHIM_PIPE%" @goto :prep
-@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"
-@if DEFINED DENO_SHIM_PIPE echo @rem \`<%=shimBinName%>\` shell pipe > "%DENO_SHIM_PIPE%"`;
-const cmdShimPrepNoPipe = `@:pipeDisabled
-@:prep
-@set "RANDOM=" &:: remove any cloak from dynamic variable RANDOM
-@if NOT DEFINED TEMP @set TEMP=%TMP%
-@if NOT EXIST "%TEMP%" @set TEMP=%TMP%
-@if NOT EXIST "%TEMP%" @goto :launch
-@set DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%
-@set DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd
-@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
-@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"`;
-
-const enablePipe = true;
-const forceUpdate = true;
-
-const cmdShimTemplate = cmdShimBase.replace(
-	'@:...prep...',
-	enablePipe ? cmdShimPrepPipe : cmdShimPrepNoPipe,
-);
 
 const isWinOS = Deno.build.os === 'windows';
 // const pathSeparator = isWinOS ? /[\\/]/ : /\//;
@@ -187,30 +123,18 @@ const fileEntries = await collect(filter(
 
 // const isEmpty = <T>(x: T) => typeof x === 'undefined' || x === null || (isString(x) && x === '');
 
+import { cmdShimTemplate, shimInfo } from '../src/lib/shim.windows.ts';
+
 const updates = await collect(map(async function (fileEntry) {
 	const shimPath = fileEntry.path;
 	const contentsOriginal = eol.LF(decoder.decode(await Deno.readFile(shimPath)));
-
-	// heuristic match for enhanced shim
-	// spell-checker:ignore () ined
-	const isEnhanced = contentsOriginal.match(/goto\s+[\W_]*undef(?:ined)?[\W_]*\s+2\s*>\s*NUL/i) ||
-		contentsOriginal.match(/shim\s*;\s*by\s*`?dxi`?/i);
-
-	const reMatchArray = contentsOriginal.match(
-		// eg, `@deno run "--allow-..." ... "https://deno.land/x/denon/denon.ts" %*`
-		/^(.*?)@\x22?deno(?:[.]exe)?\x22?\s+\x22?run\x22?\s+(.*\s+)?(\x22[^\x22]*\x22)\s+%*.*$/m,
-	) || [];
-	const [_match, _denoCommandPrefix, denoRunOptionsRaw, denoRunTarget] = reMatchArray;
-
-	const denoRunOptions = (denoRunOptionsRaw || '')
-		.replace(/((^|\s+)\x22?--\x22?)+(\s|$)/g, ' ') // remove any "--" (quoted or not); avoids collision with "--" added by template
-		.replace(/^\s+/m, '') // remove leading whitespace
-		.replace(/\s+$/m, ''); // remove trailing whitespace
 	const shimBinName = Path.parse(shimPath).name;
+	const info = shimInfo(contentsOriginal);
+	const { denoRunOptions, denoRunTarget } = info;
 	const contentsUpdated = eol.CRLF(
-		_.template(cmdShimTemplate)({ denoRunOptions, denoRunTarget, shimBinName }),
+		_.template(cmdShimTemplate(enablePipe))({ denoRunOptions, denoRunTarget, shimBinName }),
 	);
-	return { shimPath, isEnhanced, contentsUpdated, denoRunOptions, contentsOriginal };
+	return { shimPath, ...info, contentsOriginal, contentsUpdated };
 }, fileEntries));
 
 for await (const update of updates) {
