300f00d9f05dceabb0bbf1f13a8b9d41decac5fc
Roy Ivy III <rivy.dev@gmail.com>
2021-05-26 17:54:35 -0500

    docs ~ refactor eg\args.ts (cleanup, add Yargs, improve logging, ...)

--- eg/args.ts
+++ eg/args.ts
@@ -1,19 +1,75 @@
-import * as Log from 'https://deno.land/std@0.93.0/log/mod.ts';
+import * as colors from 'https://deno.land/std@0.85.0/fmt/colors.ts';
 
+import Yargs from 'https://deno.land/x/yargs@v17.0.1-deno/deno.ts';
+
+import * as LogSymbols from '../src/lib/xWait/log_symbols.ts';
 import * as Me from '../src/lib/xProcess.ts';
 
 const isWinOS = Deno.build.os === 'windows';
 
 if (isWinOS && !Me.enhanced) {
-	Log.warning(
-		'diminished capacity; full command line processing requires an enhanced runner (use `dxr` or install with `dxi`)',
+	console.warn(
+		colors.yellow(
+			'WARN: diminished capacity; full command line processing requires an enhanced runner (use `dxr` or install with `dxi`)',
+		),
 	);
 }
 
-console.log(Me.name, 'Deno:', { mainModule: Deno.mainModule, importMeta: import.meta });
-console.log(Me.name, 'xProcess:', Me);
+const runAsName = Me.shimArg0 || Me.name;
+const version = '0.0.1';
 
-const argsText = Me.argsText;
-const args = Me.args();
+// ref: <https://devhints.io/yargs> , <https://github.com/yargs/yargs/tree/v17.0.1-deno/docs>
+const app = Yargs(undefined, undefined, undefined)
+	.scriptName(Me.name)
+	.usage(
+		`$0 ${version}\n\nUsage:\n  ${runAsName} [options] [args]`,
+		undefined,
+		undefined,
+		undefined,
+	)
+	// help and version setup
+	.help(false, undefined)
+	.version(false, undefined, undefined)
+	.option('help', { describe: 'Show help', boolean: true })
+	.alias('help', 'h')
+	.option('version', { describe: 'Show version number', boolean: true })
+	.alias('version', 'V')
+	.showHelpOnFail(true, `Use \`${runAsName} --help\` to show available options`)
+	// ref: <https://github.com/yargs/yargs-parser#configuration>
+	.parserConfiguration({
+		'camel-case-expansion': true,
+		'strip-aliased': true,
+		'strip-dashed': true,
+		'unknown-options-as-args': true,
+	})
+	.option('zero', { describe: 'Display $0 (executable)', boolean: true })
+	.alias('zero', '0')
+	.option('lines', { describe: 'Display arguments on separate lines', boolean: true })
+	.alias('lines', 'l')
+	.option('debug', { describe: 'Show debug logging', boolean: true });
 
-console.log(Me.name, { argsText, args, vsDeno: Deno.args });
+const args = app.parse(Me.args(), undefined, undefined);
+
+// ref: <https://stackoverflow.com/questions/50565408/should-bash-scripts-called-with-help-argument-return-0-or-not-zero-exit-code>
+if (args.help) {
+	console.log(await app.getHelp());
+	Deno.exit(0);
+}
+if (args.version) {
+	console.log(version);
+	Deno.exit(0);
+}
+
+if (args.debug) {
+	const symbol = LogSymbols.symbolStrings.emoji.debug;
+	console.warn(symbol, Me.name, 'Deno:', { mainModule: Deno.mainModule, importMeta: import.meta });
+	console.warn(symbol, Me.name, 'xProcess:', Me);
+	console.warn(symbol, Me.name, { xProcessArgs: args._, DenoArgs: Deno.args });
+}
+
+if (args.zero) {
+	console.log('$0 =', Me.shimArg0);
+}
+
+const output = (args._ as string[]).join(args.lines ? '\n' : ' ');
+console.log(output);
