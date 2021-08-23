c814047ebb25cd2ce4a4558a5c9162e9a8a1489e
Roy Ivy III <rivy.dev@gmail.com>
2021-08-23 11:24:18 -0500

    tests/refactor ~ strengthen validator-type for type testing
    - shift type testing construction/logic errors from run-time to compile-time

--- tests/types_test.ts
+++ tests/types_test.ts
@@ -1,21 +1,26 @@
 // import { assert } from 'https://deno.land/std@0.93.0/testing/asserts.ts';
 import { writeAllSync } from 'https://deno.land/std@0.93.0/io/mod.ts';
 
-// import Schema, { array, number, string, unknown } from 'http://esm.sh/computed-types@1.6.0';
-import { string, unknown } from 'http://esm.sh/computed-types@1.6.0';
-// import type { SchemaValidatorFunction, SchemaReturnType } from 'http://esm.sh/computed-types@1.6.0';
+// import Schema, { array, number, string, unknown } from 'cdn.esm.sh/v45/computed-types@1.6.0';
+import { string, unknown } from 'https://cdn.esm.sh/v45/computed-types@1.6.0';
+// import type { SchemaValidatorFunction, SchemaReturnType } from 'cdn.esm.sh/v45/computed-types@1.6.0';
+// import type { ValidatorProxy as _ } from 'https://cdn.esm.sh/v45/computed-types@1.6.0/lib/Validator.d.ts';
 
 import { testTemplate } from './common.ts';
+// import { assertEquals } from './deps.ts';
 
 const test = testTemplate(import.meta.url);
 
-type ValidatorType = unknown;
-const isOfType = (s: ValidatorType, value: unknown) => {
+type ValidatorType = {
+	validator: (_: unknown) => unknown[];
+	destruct: () => (_: unknown) => unknown;
+}; // ref: https://github.com/neuledge/computed-types/issues/106
+const isOfType = (v: ValidatorType, value: unknown) => {
 	// deno-lint-ignore no-explicit-any
-	return ((s as unknown) as any).destruct()(value);
+	return ((v as unknown) as any).destruct()(value);
 };
-const assertType = (s: ValidatorType, value: unknown) => {
-	const result = isOfType(s, value) as [Error, unknown];
+const assertType = (v: ValidatorType, value: unknown) => {
+	const result = isOfType(v, value) as [Error, unknown];
 	if (result[0]) {
 		throw result[0];
 	}
@@ -28,16 +33,18 @@ const e = new TextEncoder();
 test('parse', () => {
 	writeAllSync(Deno.stdout, e.encode('['));
 
-	// let result;
+	// let actual;
 
 	writeAllSync(Deno.stdout, e.encode('.'));
-	// result = isOfType(unknown.array().of(string).max(0), Parse.splitByBareWS(''));
-	// console.warn({ result });
+	// actual = Parse.wordSplitCLText('');
+	// assertType(unknown.array().of(string).max(0), actual);
+	// assertEquals([], actual);
 	assertType(unknown.array().of(string).max(0), Parse.wordSplitCLText(''));
 
 	writeAllSync(Deno.stdout, e.encode('.'));
-	// result = isOfType(unknown.array().of(string), Parse.splitByBareWS('test this'));
-	// console.warn({ result });
+	// actual = Parse.wordSplitCLText('test this');
+	// assertType(unknown.array().of(string), Parse.wordSplitCLText('test this'));
+	// assertEquals(['test', 'this'], actual);
 	assertType(unknown.array().of(string), Parse.wordSplitCLText('test this'));
 
 	writeAllSync(Deno.stdout, e.encode('] '));
