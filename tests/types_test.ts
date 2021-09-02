6cbcca84f2d72b4b239a89612689e039c9e5cfee
Roy Ivy III <rivy.dev@gmail.com>
2021-09-02 16:27:48 -0500

    tests ~ reduce test output clutter

--- tests/types_test.ts
+++ tests/types_test.ts
@@ -1,5 +1,5 @@
 // import { assert } from 'https://deno.land/std@0.93.0/testing/asserts.ts';
-import { writeAllSync } from 'https://deno.land/std@0.93.0/io/mod.ts';
+// import { writeAllSync } from 'https://deno.land/std@0.93.0/io/mod.ts';
 
 // import Schema, { array, number, string, unknown } from 'cdn.esm.sh/v45/computed-types@1.6.0';
 import { string, unknown } from 'https://cdn.esm.sh/v45/computed-types@1.6.0';
@@ -28,24 +28,24 @@ const assertType = (v: ValidatorType, value: unknown) => {
 
 import * as Parse from '../src/lib/xArgs.ts';
 
-const e = new TextEncoder();
+// const e = new TextEncoder();
 
 test('parse', () => {
-	writeAllSync(Deno.stdout, e.encode('['));
+	// writeAllSync(Deno.stdout, e.encode('['));
 
 	// let actual;
 
-	writeAllSync(Deno.stdout, e.encode('.'));
+	// writeAllSync(Deno.stdout, e.encode('.'));
 	// actual = Parse.wordSplitCLText('');
 	// assertType(unknown.array().of(string).max(0), actual);
 	// assertEquals([], actual);
 	assertType(unknown.array().of(string).max(0), Parse.wordSplitCLText(''));
 
-	writeAllSync(Deno.stdout, e.encode('.'));
+	// writeAllSync(Deno.stdout, e.encode('.'));
 	// actual = Parse.wordSplitCLText('test this');
 	// assertType(unknown.array().of(string), Parse.wordSplitCLText('test this'));
 	// assertEquals(['test', 'this'], actual);
 	assertType(unknown.array().of(string), Parse.wordSplitCLText('test this'));
 
-	writeAllSync(Deno.stdout, e.encode('] '));
+	// writeAllSync(Deno.stdout, e.encode('] '));
 });
