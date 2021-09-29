688a13ef092d945b25c0c5c8876c028f91c95e61
Roy Ivy III <rivy.dev@gmail.com>
2021-09-29 11:28:04 -0500

    fix TextEncoder/Decoder dependency definitions

--- src/lib/$deps.ts
+++ src/lib/$deps.ts
@@ -9,8 +9,10 @@ import { walk, walkSync } from 'https://deno.land/std@0.106.0/fs/walk.ts';
 export const fs = { exists, existsSync, expandGlob, expandGlobSync, walk, walkSync };
 
 // export { decode, encode } from 'https://deno.land/std@0.85.0/encoding/utf8.ts'; // 'utf8.ts' was removed via commit 5bc18f5d86
-export const decode = new TextDecoder().decode;
-export const encode = new TextEncoder().encode;
+export const decoder = new TextDecoder();
+export const encoder = new TextEncoder();
+export const decode = (input?: Uint8Array): string => decoder.decode(input);
+export const encode = (input?: string): Uint8Array => encoder.encode(input);
 
 export * as Lodash from 'https://cdn.skypack.dev/pin/lodash@v4.17.20-4NISnx5Etf8JOo22u9rw/lodash.js';
 export { fetch } from 'https://deno.land/x/file_fetch@0.2.0/mod.ts';
