0db32b77cf926082bc95f00121224ac1c5b1edf3
Roy Ivy III <rivy.dev@gmail.com>
2021-09-28 19:20:52 -0500

    fix version implementation (broken for network installs)

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,15 +1,17 @@
 // VERSION handler
 
-import { Path } from './$deps.ts';
+// `fetch()` implementation (requires read or network permissions)
+// import { VERSION_PATH } from './$shared.ts';
+// import { fetch } from './$deps.ts'; // 'file://'-compatible `fetch()`
+// const newline = /\r?\n|\n/;
+// const versionURL = new URL(VERSION_PATH, import.meta.url);
+// // projectVersionText == first non-empty line (EOL trimmed) from VERSION
+// const projectVersionText =
+// 	(await (await fetch(versionURL)).text()).split(newline).filter((s) => s)[0];
 
-const versionPath = Path.toFileUrl(Path.join(Path.fromFileUrl(Deno.mainModule), '../../VERSION'));
-
-// console.warn({ versionPath });
-
-const newline = /\r?\n|\n/;
-
-// projectVersionText == first non-empty line (EOL trimmed) from VERSION
-const projectVersionText = Deno.readTextFileSync(versionPath).split(newline).filter((s) => s)[0];
+// `import ...` implementation (requires project-level synchronization tooling)
+import { VERSION } from './$shared.ts';
+const projectVersionText = VERSION;
 
 export function v() {
 	return projectVersionText;
