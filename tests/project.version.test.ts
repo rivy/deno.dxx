95dd68cc6826003a73d1e6ba5f209dacb015b65b
Roy Ivy III <rivy.dev@gmail.com>
2021-10-02 00:34:23 -0500

    refactor ~ revise project version testing based on updated deps/shared

--- tests/project.version.test.ts
+++ tests/project.version.test.ts
@@ -1,17 +1,18 @@
-import { assertEquals, decode, equal, Path } from './$deps.ts';
-import { createTestFn, createWarnFn, haveGit, projectPath } from './$shared.ts';
+import { assertEquals, decode, equal, FS } from './$deps.ts';
+const { existsSync } = FS;
+import {
+	createTestFn,
+	createWarnFn,
+	haveGit,
+	projectName,
+	projectPaths,
+	VERSION,
+} from './$shared.ts';
 
 const test = createTestFn(import.meta.url);
 const warn = createWarnFn(import.meta.url);
 
-import * as Version from '../src/lib/version.ts';
-
 const newlines = /\r?\n|\n/g;
-
-const projectFilePath = {
-	readme: Path.join(projectPath, 'README.md'),
-	version: Path.join(projectPath, 'VERSION'),
-};
 
 const gitDescribe = (await haveGit())
 	? () => {
@@ -32,13 +33,13 @@ const gitDescribe = (await haveGit())
 const gitDescribeVersion = async () =>
 	((await gitDescribe())?.match(/^v?((?:\d+[.])*\d+)/) || [])[1];
 
-if ((await haveGit()) && !equal(await gitDescribeVersion(), Version.v())) {
+if ((await haveGit()) && !equal(await gitDescribeVersion(), VERSION)) {
 	warn(
-		`\`git describe --tags\` reports the version as '${await gitDescribeVersion()}' instead of '${Version.v()}'.`,
+		`\`git describe --tags\` reports the version as '${await gitDescribeVersion()}' instead of '${VERSION}'.`,
 	);
 }
 
-// for commits tagged as releases, test for equivalence between project version and `git describe`
+// for commits tagged as releases (ie, /v?(?\d+[.])*\d+/), test for equivalence between project version and `git describe`
 // * check `GITHUB_REF` for a value such as 'refs/tags/<TAG_STRING>'
 // * ref: <https://docs.github.com/en/actions/learn-github-actions/environment-variables>
 {
@@ -48,7 +49,7 @@ if ((await haveGit()) && !equal(await gitDescribeVersion(), Version.v())) {
 		// const text = await gitDescribeVersion;
 		// testing a version tagged commit
 		test('version matches `git describe --tags`', async () => {
-			const expected = Version.v();
+			const expected = VERSION;
 			const actual = await gitDescribeVersion();
 			assertEquals(actual, expected);
 		});
@@ -56,22 +57,23 @@ if ((await haveGit()) && !equal(await gitDescribeVersion(), Version.v())) {
 }
 
 test(`version matches 'VERSION' file`, () => {
-	const expected = Version.v();
-	const actual = Deno.readTextFileSync(projectFilePath.version).replace(newlines, '');
+	const expected = VERSION;
+	const actual = Deno.readTextFileSync(projectPaths.version).replace(newlines, '');
 	assertEquals(actual, expected);
 });
 
-const readmeText = Deno.readTextFileSync(projectFilePath.readme);
-// const URLrx = /(?<=^|\s)(https://deno.land/x/dxx@)v?(?:(?:\d+[.])*\d+)(?=/)/i;
-const URLre = new RegExp('https?://deno.land/x/dxx@v?((?:\\d+[.])*\\d+)(?=/)', 'gim');
-const readmeURLs = Array.from(readmeText.matchAll(URLre));
+if ((projectName && (projectName.length > 0)) && existsSync(projectPaths.readme)) {
+	const readmeText = Deno.readTextFileSync(projectPaths.readme);
+	const URLrx = new RegExp(`https?://deno.land/x/${projectName}@v?((?:\\d+[.])*\\d+)(?=/)`, 'gim');
+	const readmeURLs = Array.from(readmeText.matchAll(URLrx));
 
-test(
-	`README ~ 'VERSION' file matches versioned URLs (${readmeURLs?.length || 'NONE'} found)`,
-	() => {
-		const expected = Version.v();
-		console.log({ URLs: readmeURLs.flatMap((v) => [{ match: v[0], index: v.index }]) });
-		readmeURLs?.forEach((v) => assertEquals(v[1], expected));
-	},
-	{ ignore: (readmeURLs?.length || 0) < 1 },
-);
+	test(
+		`README ~ 'VERSION' file matches versioned URLs (${readmeURLs?.length || 'NONE'} found)`,
+		() => {
+			const expected = VERSION;
+			console.log({ URLs: readmeURLs.flatMap((v) => [{ match: v[0], index: v.index }]) });
+			readmeURLs?.forEach((v) => assertEquals(v[1], expected));
+		},
+		{ ignore: (readmeURLs?.length || 0) < 1 },
+	);
+}
