a625cd73904712b468b25cf5521b3f86cd4ae5c9
Roy Ivy III <rivy.dev@gmail.com>
2021-10-02 00:29:13 -0500

    add project details to '$shared'

--- src/lib/$shared.ts
+++ src/lib/$shared.ts
@@ -1,2 +1,13 @@
+import { Path } from './$deps.ts';
+
 export const VERSION = '0.0.9';
-export const VERSION_PATH = 'VERSION'; // path is relative to project base directory
+
+export const projectName: string | undefined = 'dxx';
+export const projectURL = new URL('../..', import.meta.url); // note: `new URL('.', ...)` => dirname(...); `new URL('..', ...) => dirname(dirname(...))
+export const projectPath = Path.fromFileUrl(projectURL);
+export const projectPaths = {
+	// absolute or relative to `projectPath`
+	editorconfig: Path.join(projectPath, '.editorconfig'),
+	readme: Path.join(projectPath, 'README.md'),
+	version: Path.join(projectPath, 'VERSION'),
+};
