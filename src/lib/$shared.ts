95fc03ba2fb559bb19639c46175b84fab455824f
Roy Ivy III <rivy.dev@gmail.com>
2021-10-20 00:00:35 -0500

    fix `projectPath` calculation for browser/URL-based imports

--- src/lib/$shared.ts
+++ src/lib/$shared.ts
@@ -1,10 +1,14 @@
+// spell-checker:ignore (names) EditorConfig
+
 import { Path } from './$deps.ts';
 
 export const VERSION = '0.0.9';
 
 export const projectName: string | undefined = 'dxx';
 export const projectURL = new URL('../..', import.meta.url); // note: `new URL('.', ...)` => dirname(...); `new URL('..', ...) => dirname(dirname(...))
-export const projectPath = Path.fromFileUrl(projectURL);
+export const projectPath = (projectURL.protocol === 'file:')
+	? Path.fromFileUrl(projectURL)
+	: projectURL.pathname;
 export const projectPaths = {
 	// absolute or relative to `projectPath`
 	editorconfig: Path.join(projectPath, '.editorconfig'),
