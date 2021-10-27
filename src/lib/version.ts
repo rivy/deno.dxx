0a7aaf56872ecc9ea6c409ef0bf30fa9c99908d2
Roy Ivy III <rivy.dev@gmail.com>
2021-10-26 21:46:59 -0500

    refactor ~ add portable (local/network) version calculation from 'VERSION' file

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,17 +1,18 @@
 // VERSION handler
 
-// `fetch()` implementation (requires read or network permissions)
-// import { VERSION_PATH } from './$shared.ts';
-// import { fetch } from './$deps.ts'; // 'file://'-compatible `fetch()`
-// const newline = /\r?\n|\n/;
-// const versionURL = new URL(VERSION_PATH, import.meta.url);
-// // projectVersionText == first non-empty line (EOL trimmed) from VERSION
-// const projectVersionText =
-// 	(await (await fetch(versionURL)).text()).split(newline).filter((s) => s)[0];
+// `fetch()` implementation (requires read [for local runs] or network permissions)
+import { intoURL, projectPaths, projectURL } from '../../tests/$shared.ts';
+import { fetch } from './$deps.ts'; // 'file://'-compatible `fetch()`
+const newline = /\r?\n|\n/;
+const versionURL = intoURL(projectPaths.version, projectURL);
+console.log({ projectURL, projectPaths, versionURL });
+// projectVersionText == first non-empty line (EOL trimmed) from VERSION
+export const projectVersionText = versionURL &&
+	(await (await fetch(versionURL)).text()).split(newline).filter((s) => s)[0];
 
 // `import ...` implementation (requires project-level synchronization tooling)
 import { VERSION } from './$shared.ts';
-const projectVersionText = VERSION;
+export const projectVersionTextViaImport = VERSION;
 
 export function v() {
 	return projectVersionText;
