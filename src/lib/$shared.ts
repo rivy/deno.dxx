d54d4d13bd71b6a856b45b2b6a3e7c463aca1050
Roy Ivy III <rivy.dev@gmail.com>
2021-11-20 13:04:46 -0600

    refactor/$shared ~ use `$module` naming idiom/scheme

--- src/lib/$shared.ts
+++ src/lib/$shared.ts
@@ -2,7 +2,7 @@
 // spell-checker:ignore (names) Deno EditorConfig
 // spell-checker:ignore (modules) stringz
 
-import { Path } from './$deps.ts';
+import { $path } from './$deps.ts';
 
 //===
 
@@ -11,12 +11,12 @@ export const VERSION = '0.0.9';
 
 export const projectURL = new URL('../..', import.meta.url); // note: `new URL('.', ...)` => dirname(...); `new URL('..', ...) => dirname(dirname(...))
 export const projectPath =
-	((url: URL) => (url.protocol === 'file:') ? Path.fromFileUrl(url) : url.pathname)(projectURL);
+	((url: URL) => (url.protocol === 'file:') ? $path.fromFileUrl(url) : url.pathname)(projectURL);
 export const projectPaths = {
 	// absolute or relative to `projectPath`
-	editorconfig: Path.join(projectPath, '.editorconfig'),
-	readme: Path.join(projectPath, 'README.md'),
-	version: Path.join(projectPath, 'VERSION'),
+	editorconfig: $path.join(projectPath, '.editorconfig'),
+	readme: $path.join(projectPath, 'README.md'),
+	version: $path.join(projectPath, 'VERSION'),
 };
 
 // // ToDO: investigate best practice for portability of PATH_SEP_PATTERN // note: WinOS => /[\\/]+/ ; *nix => /\/+/
@@ -42,11 +42,11 @@ export function isFileURL(url: URL) {
 	return (url.protocol === 'file:');
 }
 
-export function isValidURL(s: string, base: URL = Path.toFileUrl(Deno.cwd() + Path.SEP)) {
+export function isValidURL(s: string, base: URL = $path.toFileUrl(Deno.cwd() + $path.SEP)) {
 	return !!validURL(s, base);
 }
 
-export function validURL(s: string, base: URL = Path.toFileUrl(Deno.cwd() + Path.SEP)) {
+export function validURL(s: string, base: URL = $path.toFileUrl(Deno.cwd() + $path.SEP)) {
 	return intoURL(s, base);
 }
 
@@ -56,7 +56,7 @@ export function validURL(s: string, base: URL = Path.toFileUrl(Deno.cwd() + Path
 /** Extract a path string from a path string (identity function) or URL. */
 export function intoPath(path: string | URL) {
 	if (!(path instanceof URL)) return path;
-	return (path.protocol === 'file:') ? Path.fromFileUrl(path) : path.pathname;
+	return (path.protocol === 'file:') ? $path.fromFileUrl(path) : path.pathname;
 }
 
 // ref: <https://en.wikipedia.org/wiki/Uniform_Resource_Identifier> , <https://stackoverflow.com/questions/48953298/whats-the-difference-between-a-scheme-and-a-protocol-in-a-url>
@@ -69,7 +69,7 @@ const IntoUrlOptionsDefault: Required<IntoUrlOptions> = { driveLetterSchemes: tr
 /** Convert a `path` string into an URL, relative to a `base` URL.
 
 @param [path]
-@param [base] • baseline URL ~ defaults to `Path.toFileUrl(Deno.cwd()+Path.SEP)`; _note_: per usual relative URL rules, if `base` does not have a trailing separator, determination of path is relative the _the parent of `base`_
+@param [base] • baseline URL ~ defaults to `$path.toFileUrl(Deno.cwd()+$path.SEP)`; _note_: per usual relative URL rules, if `base` does not have a trailing separator, determination of path is relative the _the parent of `base`_
 @param [options] ~ defaults to `{driveLetterSchemes: true}`
 */
 export function intoURL(path: string, base?: URL, options?: IntoUrlOptions): URL | undefined;
@@ -77,14 +77,14 @@ export function intoURL(path: string, options: IntoUrlOptions): URL | undefined;
 export function intoURL(path: string, ...args: unknown[]) {
 	const base = (args?.length > 0 && (args[0] instanceof URL))
 		? args.shift() as URL
-		: Path.toFileUrl(Deno.cwd() + Path.SEP);
+		: $path.toFileUrl(Deno.cwd() + $path.SEP);
 	const options = {
 		...IntoUrlOptionsDefault,
 		...(args?.length > 0) ? args.shift() as IntoUrlOptions : {},
 	};
 	const scheme = (path.match(/^[A-Za-z][A-Za-z0-9+-.]*(?=:)/) || [])[0]; // per [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986#section-3.1)
 	if (options.driveLetterSchemes && scheme?.length == 1) {
-		path = 'file://' + Path.normalize(path);
+		path = 'file://' + $path.normalize(path);
 	}
 	// console.warn({ path, base, options });
 	try {
@@ -103,11 +103,11 @@ _Returned path will be relative if `goal` shares a common origin/prefix with `ba
 - _relative `goal` or `base` paths are evaluated relative to the `Deno.cwd()` directory_
 
 @param [goal] • target path
-@param [base] • starting path ~ defaults to `Path.toFileUrl(Deno.cwd()+Path.SEP)`; _note_: per usual relative URL rules, if `base` does not have a trailing separator, determination of path is relative the _the parent of `base`_
+@param [base] • starting path ~ defaults to `$path.toFileUrl(Deno.cwd()+$path.SEP)`; _note_: per usual relative URL rules, if `base` does not have a trailing separator, determination of path is relative the _the parent of `base`_
 */
 export function traversal(
 	goal: string | URL,
-	base: string | URL = Path.toFileUrl(Deno.cwd() + Path.SEP),
+	base: string | URL = $path.toFileUrl(Deno.cwd() + $path.SEP),
 ) {
 	const url = (goal instanceof URL) ? goal : intoURL(goal);
 	const baseURL = (base instanceof URL) ? base : intoURL(base);
@@ -115,7 +115,7 @@ export function traversal(
 		(url.origin.localeCompare(baseURL.origin, undefined, { sensitivity: 'accent' }) == 0);
 	// console.warn({ goal, source, url, baseURL, commonOrigin });
 	if (url && baseURL && commonOrigin) {
-		return Path.relative(baseURL.pathname, url.pathname);
+		return $path.relative(baseURL.pathname, url.pathname);
 	} else {
 		return url ? url.href : undefined;
 	}
