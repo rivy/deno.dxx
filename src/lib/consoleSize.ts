fc835a588d5071360cde86f7871c358ade00cc1b
Roy Ivy III <rivy.dev@gmail.com>
2021-11-27 16:41:13 -0600

    refactor/consoleSize ~ improve efficiency by revising promise construction and use

--- src/lib/consoleSize.ts
+++ src/lib/consoleSize.ts
@@ -13,37 +13,43 @@ export function consoleSize(
 		conoutFallback: true,
 	},
 ): Promise<ConsoleSize | undefined> {
-	const retVal = Promise
-		.any([
-			new Promise<ConsoleSize | undefined>((resolve, reject) => {
-				consoleSizeViaDeno(rid, options).then((size) =>
-					(size != undefined) ? resolve(size) : reject(undefined)
-				);
-			}),
-			new Promise<ConsoleSize | undefined>((resolve, reject) => {
-				consoleSizeViaPowerShell().then((size) =>
-					(size != undefined) ? resolve(size) : reject(undefined)
-				);
-			}),
-			new Promise<ConsoleSize | undefined>((resolve, reject) => {
-				consoleSizeViaSTTY().then((size) =>
-					(size != undefined) ? resolve(size) : reject(undefined)
-				);
-			}),
-			new Promise<ConsoleSize | undefined>((resolve, reject) => {
-				consoleSizeViaTPUT().then((size) =>
-					(size != undefined) ? resolve(size) : reject(undefined)
-				);
-			}),
-		])
-		.catch((_) => undefined);
+	// attempt fast API first, with fallback to slower shell scripts
+	// * paying for construction and execution only if needed by using `catch()` as fallback and/or `then()` for the function calls
+	const promise = new Promise<ConsoleSize | undefined>((resolve, reject) => {
+		// ~ 5 ms for WinOS ; ~ 0.5 ms for POSIX (for open, un-redirected STDOUT or STDERR)
+		// ~ 150 ms for WinOS ; ~ 75 ms for POSIX (when requiring use of the consoleFileFallback)
+		consoleSizeViaDenoAPI(rid, options).then((size) =>
+			(size != undefined) ? resolve(size) : reject(undefined)
+		);
+	})
+		.catch((_) =>
+			// shell script fallbacks
+			// ~ 150 ms for WinOS ; ~ 75 ms for POSIX
+			// * Promise constructors are synchronously eager, but `.then(...)/.catch(...)` is guaranteed to execute on the async stack
+			// ref: https://medium.com/@mpodlasin/3-most-common-mistakes-in-using-promises-in-javascript-575fc31939b6 @@ <https://archive.is/JmH5N>
+			// ref: https://medium.com/@mpodlasin/promises-vs-observables-4c123c51fe13 @@ <https://archive.is/daGxV>
+			// ref: https://stackoverflow.com/questions/21260602/how-to-reject-a-promise-from-inside-then-function
+			Promise
+				.any([
+					consoleSizeViaPowerShell().then((size) =>
+						(size != undefined) ? size : Promise.reject(undefined)
+					),
+					consoleSizeViaSTTY().then((size) =>
+						(size != undefined) ? size : Promise.reject(undefined)
+					),
+					consoleSizeViaTPUT().then((size) =>
+						(size != undefined) ? size : Promise.reject(undefined)
+					),
+				])
+				.catch((_) => undefined)
+		);
 
-	return retVal;
+	return promise;
 }
 
 //===
 
-export async function consoleSizeViaDeno(
+export function consoleSizeViaDenoAPI(
 	rid: number = Deno.stdout.rid,
 	options: { fallbackRIDs: number[]; conoutFallback: boolean } = {
 		fallbackRIDs: [Deno.stderr.rid],
@@ -52,12 +58,10 @@ export async function consoleSizeViaDeno(
 ): Promise<ConsoleSize | undefined> {
 	// `Deno.consoleSize()` is unstable API (as of v1.12+) => deno-lint-ignore no-explicit-any
 	// deno-lint-ignore no-explicit-any
-	const denoConsoleSize = (Deno as any)?.consoleSize as (
-		rid: number,
-	) => { columns: number; rows: number } | undefined;
-	if (denoConsoleSize == undefined) return undefined;
+	const denoConsoleSize = (Deno as any).consoleSize as (rid: number) => ConsoleSize | undefined;
+	if (denoConsoleSize == undefined) return Promise.resolve(undefined);
 
-	let size: { columns: number; rows: number } | undefined;
+	let size: ConsoleSize | undefined;
 	try {
 		// * `denoConsoleSize()` throws if rid is redirected
 		size = denoConsoleSize?.(rid);
@@ -76,22 +80,24 @@ export async function consoleSizeViaDeno(
 	}
 
 	if ((size == undefined) && options.conoutFallback) {
-		try {
-			// ref: https://unix.stackexchange.com/questions/60641/linux-difference-between-dev-console-dev-tty-and-dev-tty0
-			const conOut = await Deno.open(isWinOS ? 'CONOUT$' : '/dev/tty');
-			// * `denoConsoleSize()` throws if rid is redirected
-			size = conOut && denoConsoleSize?.(conOut.rid);
-			conOut.close();
-		} catch {
-			size = undefined;
-		}
+		// ref: https://unix.stackexchange.com/questions/60641/linux-difference-between-dev-console-dev-tty-and-dev-tty0
+		Deno
+			.open(isWinOS ? 'CONOUT$' : '/dev/tty')
+			.then((file) => {
+				try {
+					size = denoConsoleSize?.(file.rid);
+				} finally {
+					file.close();
+				}
+			})
+			.catch((_) => size = undefined);
 	}
 
-	return size;
+	return Promise.resolve(size);
 }
 
-export async function consoleSizeViaPowerShell(): Promise<ConsoleSize | undefined> {
-	const output = await (() => {
+export function consoleSizeViaPowerShell(): Promise<ConsoleSize | undefined> {
+	const output = (() => {
 		try {
 			const process = Deno.run({
 				cmd: [
@@ -111,18 +117,23 @@ export async function consoleSizeViaPowerShell(): Promise<ConsoleSize | undefine
 		} catch (_) {
 			return Promise.resolve(undefined);
 		}
-	})() ?? '';
-	const values = output.split(/\s+/).filter((s) => s.length > 0);
-	return values.length > 0
-		? { columns: Number(values.shift()), rows: Number(values.shift()) }
-		: undefined;
+	})();
+
+	const promise = output.then((text) => text?.split(/\s+/).filter((s) => s.length > 0) ?? []).then((
+		values,
+	) =>
+		values.length > 0
+			? { columns: Number(values.shift()), rows: Number(values.shift()) }
+			: undefined
+	);
+	return promise;
 }
 
-export async function consoleSizeViaSTTY(): Promise<ConsoleSize | undefined> {
+export function consoleSizeViaSTTY(): Promise<ConsoleSize | undefined> {
 	// * note: `stty size` depends on a TTY connected to STDIN; ie, `stty size </dev/null` will fail
 	// * note: On Windows, `stty size` causes odd end of line word wrap abnormalities for lines containing ANSI escapes => avoid
-	if (isWinOS) return undefined;
-	const output = await (() => {
+	if (isWinOS) return Promise.resolve(undefined);
+	const output = (() => {
 		try {
 			const process = Deno.run({
 				cmd: ['stty', 'size', 'sane'],
@@ -134,16 +145,21 @@ export async function consoleSizeViaSTTY(): Promise<ConsoleSize | undefined> {
 		} catch (_) {
 			return Promise.resolve(undefined);
 		}
-	})() ?? '';
-	const values = output.split(/\s+/).filter((s) => s.length > 0).reverse();
-	return values.length > 0
-		? { columns: Number(values.shift()), rows: Number(values.shift()) }
-		: undefined;
+	})();
+
+	const promise = output
+		.then((text) => text?.split(/\s+/).filter((s) => s.length > 0).reverse() ?? [])
+		.then((values) =>
+			values.length > 0
+				? { columns: Number(values.shift()), rows: Number(values.shift()) }
+				: undefined
+		);
+	return promise;
 }
 
-async function consoleSizeViaTPUT(): Promise<ConsoleSize | undefined> {
-	// * note: `tput` is resilient to STDIN, STDOUT, and STDERR redirects, but requires two shell system calls
-	const cols = await (() => {
+export function consoleSizeViaTPUT(): Promise<ConsoleSize | undefined> {
+	// * note: `tput` is resilient to STDIN, STDOUT, and STDERR redirects, but requires two system shell calls
+	const colsOutput = (() => {
 		try {
 			const process = Deno.run({
 				cmd: ['tput', 'cols'],
@@ -155,8 +171,8 @@ async function consoleSizeViaTPUT(): Promise<ConsoleSize | undefined> {
 		} catch (_) {
 			return Promise.resolve(undefined);
 		}
-	})() ?? '';
-	const lines = await (() => {
+	})();
+	const linesOutput = (() => {
 		try {
 			const process = Deno.run({
 				cmd: ['tput', 'lines'],
@@ -168,10 +184,17 @@ async function consoleSizeViaTPUT(): Promise<ConsoleSize | undefined> {
 		} catch (_) {
 			return Promise.resolve(undefined);
 		}
-	})() ?? '';
-	return (cols.length > 0 && lines.length > 0)
-		? { columns: Number(cols), rows: Number(lines) }
-		: undefined;
+	})();
+
+	const promise = Promise
+		.all([colsOutput, linesOutput])
+		.then(([colsText, linesText]) => [colsText ?? '', linesText ?? ''])
+		.then(([cols, lines]) =>
+			(cols.length > 0 && lines.length > 0)
+				? { columns: Number(cols), rows: Number(lines) }
+				: undefined
+		);
+	return promise;
 }
 
 // const consoleSizes = {
