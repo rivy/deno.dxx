0c4997eb586ef3e9da364ab7f941a94b5f5def17
Roy Ivy III <rivy.dev@gmail.com>
2022-01-21 19:08:20 -0600

    change windows shim template to use `SHIM_...` environment variables

--- src/lib/shim.windows.ts
+++ src/lib/shim.windows.ts
@@ -5,33 +5,31 @@ const cmdShimBase = `% \`<%=shimBinName%>\` (*enhanced* Deno CMD shim; by \`dxi\
 @set "ERRORLEVEL="
 @set "DENO_SHIM_ERRORLEVEL="
 @setLocal
-@set "DENO_SHIM_PIPE="
-@set "DENO_SHIM_ARGS="
-@set "DENO_SHIM_ARGX="
-@set "DENO_SHIM_URL="
+@set "SHIM_PIPE="
+@set "SHIM_ARGS="
+@set "SHIM_TARGET="
 @:...prep...
 @:launch
-@rem:: DENO_SHIM_EXEC convolution is to avoid \`%*\` within the final parse group [o/w parens within args may cause parsing/execution misbehavior]
-@>>"%DENO_SHIM_EXEC%" echo @set DENO_SHIM_ARGS=%*
-@>>"%DENO_SHIM_EXEC%" echo @goto _undef_ 2^>NUL ^|^| @for %%%%G in ("%COMSPEC%") do @title %%%%~nG ^& @deno.exe "run" <%= denoRunOptions ? (denoRunOptions + ' ') : '' %>-- "<%=denoRunTarget%>" %%DENO_SHIM_ARGS%%
+@rem:: SHIM_EXEC circumlocution is to avoid \`%*\` within the final parse group [o/w parens within args may cause parsing/execution misbehavior]
+@>>"%SHIM_EXEC%" echo @set SHIM_ARGS=%*
+@>>"%SHIM_EXEC%" echo @goto _undef_ 2^>NUL ^|^| @for %%%%G in ("%COMSPEC%") do @title %%%%~nG ^& @deno.exe "run" <%= denoRunOptions ? (denoRunOptions + ' ') : '' %>-- "<%=denoRunTarget%>" %%SHIM_ARGS%%
 @(
 @goto _undef_ 2>NUL
 @for %%G in ("%COMSPEC%") do @title %%~nG
-@set "DENO_SHIM_EXEC=%DENO_SHIM_EXEC%"
-@set "DENO_SHIM_PIPE=%DENO_SHIM_PIPE%"
-@set "DENO_SHIM_ARG0=%~0"
-@set "DENO_SHIM_URL=<%=denoRunTarget%>"
-@call "%DENO_SHIM_EXEC%"
+@set "SHIM_EXEC=%SHIM_EXEC%"
+@set "SHIM_PIPE=%SHIM_PIPE%"
+@set "SHIM_ARGV0=%~0"
+@set "SHIM_TARGET=<%=denoRunTarget%>"
+@call "%SHIM_EXEC%"
 @call set DENO_SHIM_ERRORLEVEL=%%ERRORLEVEL%%
-@if EXIST "%DENO_SHIM_PIPE%" call "%DENO_SHIM_PIPE%" >NUL 2>NUL
-@if EXIST "%DENO_SHIM_EXEC%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_EXEC%" 2>NUL
-@if EXIST "%DENO_SHIM_PIPE%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%DENO_SHIM_PIPE%" 2>NUL
-@set "DENO_SHIM_EXEC="
-@set "DENO_SHIM_PIPE="
-@set "DENO_SHIM_ARG0="
-@set "DENO_SHIM_ARGS="
-@set "DENO_SHIM_ARGX="
-@set "DENO_SHIM_URL="
+@if EXIST "%SHIM_PIPE%" call "%SHIM_PIPE%" >NUL 2>NUL
+@if EXIST "%SHIM_EXEC%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%SHIM_EXEC%" 2>NUL
+@if EXIST "%SHIM_PIPE%" if NOT DEFINED DENO_SHIM_DEBUG del /q "%SHIM_PIPE%" 2>NUL
+@set "SHIM_EXEC="
+@set "SHIM_PIPE="
+@set "SHIM_ARGV0="
+@set "SHIM_ARGS="
+@set "SHIM_TARGET="
 @call %COMSPEC% /d/c "exit %%DENO_SHIM_ERRORLEVEL%%"
 )
 `;
@@ -42,12 +40,12 @@ const cmdShimPrepPipe = `@:pipeEnabled
 @if NOT EXIST "%TEMP%" @set "TEMP=%TMP%"
 @if NOT EXIST "%TEMP%" @goto :launch
 @set "DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%"
-@set "DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd"
-@set "DENO_SHIM_PIPE=%TEMP%\\<%=shimBinName%>.shim.pipe.%DENO_SHIM_TID%.cmd"
-@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
-@if EXIST "%DENO_SHIM_PIPE%" @goto :prep
-@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"
-@if DEFINED DENO_SHIM_PIPE echo @rem \`<%=shimBinName%>\` shell pipe > "%DENO_SHIM_PIPE%"`;
+@set "SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd"
+@set "SHIM_PIPE=%TEMP%\\<%=shimBinName%>.shim.pipe.%DENO_SHIM_TID%.cmd"
+@if EXIST "%SHIM_EXEC%" @goto :prep
+@if EXIST "%SHIM_PIPE%" @goto :prep
+@if DEFINED SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%SHIM_EXEC%"
+@if DEFINED SHIM_PIPE echo @rem \`<%=shimBinName%>\` shell pipe > "%SHIM_PIPE%"`;
 const cmdShimPrepNoPipe = `@:pipeDisabled
 @:prep
 @set "RANDOM=" &:: remove any cloak from dynamic variable RANDOM
@@ -55,9 +53,9 @@ const cmdShimPrepNoPipe = `@:pipeDisabled
 @if NOT EXIST "%TEMP%" @set "TEMP=%TMP%"
 @if NOT EXIST "%TEMP%" @goto :launch
 @set "DENO_SHIM_TID=%RANDOM%.%RANDOM%.%RANDOM%"
-@set "DENO_SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd"
-@if EXIST "%DENO_SHIM_EXEC%" @goto :prep
-@if DEFINED DENO_SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%DENO_SHIM_EXEC%"`;
+@set "SHIM_EXEC=%TEMP%\\<%=shimBinName%>.shim.exec.%DENO_SHIM_TID%.cmd"
+@if EXIST "%SHIM_EXEC%" @goto :prep
+@if DEFINED SHIM_EXEC echo @rem \`<%=shimBinName%>\` shell exec > "%SHIM_EXEC%"`;
 
 export function cmdShimTemplate(enablePipe: boolean) {
 	return cmdShimBase.replace('@:...prep...', enablePipe ? cmdShimPrepPipe : cmdShimPrepNoPipe);
@@ -73,8 +71,8 @@ export function shimInfo(contentsOriginal: string) {
 		// match `deno run` options, run-target (as a URL-like quoted string), and run-target arguments from shim text
 		// * run-target is matched as the first double-quoted URL-like (like "<scheme>:...") argument
 		// eg, `deno install ...` => `@deno run "--allow-..." ... "https://deno.land/x/denon/denon.ts" %*`
-		// eg, `dxi ...` => `... @deno run "--allow-..." ... "https://deno.land/x/denon/denon.ts" %%DENO_SHIM_ARGS%%`
-		/^(.*?)@\x22?deno(?:[.]exe)?\x22?\s+\x22?run\x22?\s+(.*\s+)?\x22([a-z][a-z0-9+.-]+:[^\x22]+)\x22\s+(%[*]|%%DENO_SHIM_ARGS%%)\s*$/m,
+		// eg, `dxi ...` => `... @deno run "--allow-..." ... "https://deno.land/x/denon/denon.ts" %%SHIM_ARGS%%`
+		/^(.*?)@\x22?deno(?:[.]exe)?\x22?\s+\x22?run\x22?\s+(.*\s+)?\x22([a-z][a-z0-9+.-]+:[^\x22]+)\x22\s+(%[*]|%%(?:DENO_)SHIM_ARGS%%)\s*$/m,
 	) || [];
 	const [_match, _denoCommandPrefix, denoRunOptionsRaw, denoRunTarget, _denoRunTargetArgs] =
 		reMatchArray;
