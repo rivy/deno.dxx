def2d1b1b4680c91031a1bb86b8bb1037bfd25e5
Roy Ivy III <rivy.dev@gmail.com>
2022-06-12 19:43:53 -0500

    update ~ vendor types for 'picomatch' (required for fix of JSDelivr type load failues)

---
+++ src/@types/picomatch@2.3/lib/parse.d.ts
@@ -0,0 +1,35 @@
+// deno-lint-ignore-file
+
+declare function parse(
+	input: string,
+	options: { maxLength?: number | undefined },
+): parse.ParseState;
+
+declare namespace parse {
+	interface Token {
+		type: string;
+		value: string;
+		output: any;
+	}
+
+	interface ParseState {
+		input: string;
+		index: number;
+		start: number;
+		dot: boolean;
+		consumed: string;
+		output: string;
+		prefix: string;
+		backtrack: boolean;
+		negated: boolean;
+		negatedExtglob?: boolean | undefined;
+		brackets: number;
+		braces: number;
+		parens: number;
+		quotes: number;
+		globstar: boolean;
+		tokens: Token[];
+	}
+}
+
+export = parse;
