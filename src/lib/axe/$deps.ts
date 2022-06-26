d0610d3a7c4a9f104c362c6facfec984287418ee
Roy Ivy III <rivy.dev@gmail.com>
2022-06-26 11:13:21 -0500

    refactor/axe ~ use vendored `format()` to avoid deno::std format bugs
    - revisit when <https://github.com/denoland/deno_std/issues/2388> is addressed
    - note: this implementation varies from NodeJS equivalency because of a variation in
      `Deno.inspect()` which prioritizes double quotes over single quotes for string
      formatting
      - ref: [🙏`Deno.inspect()` should conform to NodeJS quoting behavior (or allow customization)](https://github.com/denoland/deno/issues/14969)

--- src/lib/axe/$deps.ts
+++ src/lib/axe/$deps.ts
@@ -29,8 +29,9 @@ export class PQueue extends Queue {
 // import deepMerge from 'https://cdn.esm.sh/v54/deepmerge@4.2.2/es2021/deepmerge.js';
 export { default as deepMerge } from 'https://cdn.esm.sh/v54/deepmerge@4.2.2/es2021/deepmerge.js';
 
-export { format } from 'https://deno.land/std@0.143.0/node/util.ts';
 export * as $symbols from '../xWait/log_symbols.ts';
+
+export { format } from './format.ts';
 
 export * as $levels from 'https://cdn.esm.sh/rfc-log-levels@3.17.0';
 export { default as rfcGetLogLevel } from 'https://cdn.esm.sh/rfc-log-levels@3.17.0';
