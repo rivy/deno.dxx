3e9bbbb747dd7642dc418e906a73a2d2cdfe1db8
Roy Ivy III <rivy.dev@gmail.com>
2022-06-26 13:43:07 -0500

    wip/distribute

--- eg/distribute.ts
+++ eg/distribute.ts
@@ -1,6 +1,6 @@
 // spell-checker:ignore (names) Deno ; (vars) ARGX LOGLEVEL PATHEXT arr gmsu ; (utils) dprint dprintrc ; (yargs) nargs positionals
 
-import { $yargs, YargsArguments } from '../src/lib/$deps.ts';
+import { $yargs, fetch, YargsArguments } from '../src/lib/$deps.ts';
 import { $version, durationText, env } from '../src/lib/$shared.ts';
 
 import { restyleYargsHelp } from '../src/lib/restyleYargsHelp.ts';
@@ -8,7 +8,11 @@ import { restyleYargsHelp } from '../src/lib/restyleYargsHelp.ts';
 import { $consoleSize, $me } from '../src/lib/$locals.ts';
 import {
 	$logger,
+	intoPath,
+	intoURL,
 	logger as log, /* initialized to the suspended state */
+	traversal,
+	validURL,
 } from '../src/lib/$shared.ts';
 
 //===
@@ -51,13 +55,13 @@ const app = $yargs(/* argv */ undefined, /* cwd */ undefined)
 	.scriptName(appName)
 	.epilog('* Copyright (c) 2021-2022 * Roy Ivy III (MIT license)')
 	.usage(`$0 ${version}\n
-Distribute SOURCE to TARGET(s).\n
+Distribute/update semver SOURCE to TARGET(s).\n
 Usage:\n  ${runAsName} [OPTION..] SOURCE TARGET..`)
 	// ref: <https://github.com/yargs/yargs/blob/59a86fb83cfeb8533c6dd446c73cf4166cc455f2/locales/en.json>
 	.updateStrings({ 'Positionals:': 'Arguments:' })
 	.positional('OPTION', { describe: 'OPTION(s); see listed *Options*' })
-	.positional('SOURCE', { describe: `SOURCE file to distribute`, demand: 'true' })
-	.positional('TARGET', { describe: `TARGET file(s) of distribution`, demand: 'true' })
+	.positional('SOURCE', { describe: `SOURCE file to distribute`, demand: 1 })
+	.positional('TARGET', { describe: `TARGET file(s) of distribution`, demand: 1 })
 	.fail((msg: string, err: Error, _: ReturnType<typeof $yargs>) => {
 		if (err) throw err;
 		throw new Error(msg);
@@ -117,7 +121,13 @@ Usage:\n  ${runAsName} [OPTION..] SOURCE TARGET..`)
 	})
 	// .example(`\`${runAsName} ARG\``, 'Display 'shell-expanded ARG')
 	/* Options... */
-	.strictOptions(/* enable */ true); // .demandOption(['SOURCE']);
+	.strictOptions(/* enable */ true)
+	.option('version-regex', {
+		describe: 'Regular expression used to match SOURCE and TARGET versions',
+		default: '^\\s*(#|//)\s*v\\d+(\\.\\d+)*',
+		type: 'string',
+	})
+	.alias('version-regex', ['r']); // .demandOption(['SOURCE']);
 
 performance.mark('setup:yargs:stop');
 
@@ -127,22 +137,14 @@ performance.mark('setup:parseArgs:start');
 
 const bakedArgs = $me.args();
 
-const optionArgs = bakedArgs;
-const nonOptionArgs: typeof bakedArgs = [];
-
-log.trace({ optionArgs, nonOptionArgs });
-
 const argv = ((() => {
 	try {
-		return app.parse(optionArgs) as YargsArguments;
+		return app.parse(bakedArgs) as YargsArguments;
 	} catch (e) {
 		log.error(e.message);
 		return;
 	}
 })());
-if (argv && Array.isArray(argv._)) {
-	argv._.push(...nonOptionArgs);
-}
 
 log.trace({ bakedArgs, argv });
 
@@ -186,13 +188,6 @@ performance.mark('setup:parseArgs:stop');
 
 //===
 
-if (argv == undefined) {
-	console.warn(`\nUse \`${runAsName} --help\` to show full usage and available options`);
-	Deno.exit(1);
-}
-
-//===
-
 performance.mark('setup:consoleSize');
 performance.mark('setup:consoleSize:promise');
 const consoleSizePromise = $consoleSize.consoleSize();
@@ -219,7 +214,7 @@ await log.debug(durationText('setup'));
 //===
 
 // ref: <https://stackoverflow.com/questions/50565408/should-bash-scripts-called-with-help-argument-return-0-or-not-zero-exit-code>
-if (argv.help) {
+if (argv?.help) {
 	performance.mark('run:generateHelp');
 	performance.mark('run:generateHelp:yargs');
 	const yargsHelp = await app.getHelp();
@@ -236,7 +231,7 @@ if (argv.help) {
 		Object.keys(argv).filter((s) => !['help', '_', '$0'].includes(s)).length === 0;
 	Deno.exit(onlyHelp ? 0 : 1);
 }
-if (argv.version) {
+if (argv?.version) {
 	console.log(`${appName} ${version}`);
 	const onlyVersion = (argv._.length === 0) &&
 		Object.keys(argv).filter((s) => !['version', '_', '$0'].includes(s)).length === 0;
@@ -245,11 +240,72 @@ if (argv.version) {
 
 //===
 
-if (argv.zero) {
-	console.log('$0 =', $me.argv0);
+let usageError = false;
+
+// positional arguments
+// * SOURCE
+const SOURCE = await (async () => {
+	const source = argv?._?.shift()?.toString();
+	if (source == null) {
+		usageError = true;
+		await log.error(`SOURCE is a required argument`);
+	}
+	return source || '';
+})();
+
+// * TARGET..
+const TARGET = await (async () => {
+	const target = argv ? [...argv._] : [];
+	if (argv) argv._ = [];
+	if (target.length < 1) {
+		usageError = true;
+		await log.error(`TARGET is a required argument`);
+	}
+	return target.map((e) => e.toString());
+})();
+
+// argument/option parsing or usage error
+if (!argv || usageError) {
+	console.warn(`\nUse \`${runAsName} --help\` to show full usage and available options`);
+	Deno.exit(1);
 }
 
-const output = (argv._.map(String))?.join(argv.lines ? '\n' : ' ');
-if (output.length > 0) {
-	console.log(output);
+//===
+
+console.log("SOURCE = '%s' (aka: '%s')", validURL(SOURCE)?.href, traversal(SOURCE));
+console.log(
+	'TARGET = %O (aka: %O)',
+	TARGET.map((e) => validURL(e)?.href),
+	TARGET.map((e) => traversal(e)),
+);
+
+TARGET.forEach((dest) => {
+	remoteCopy(ensureURL(SOURCE), ensureURL(dest));
+});
+
+//===
+
+function ensureURL(path: string | URL) {
+	if (path instanceof URL) return path;
+	const url = intoURL(path);
+	if (url == null) throw 'ToDO-ERROR-type';
+	return url;
+}
+
+function ensurePath(path?: string | URL) {
+	const p = intoPath(path);
+	if (p == null || p === '') throw 'ToDO-ERROR-type';
+	return p;
+}
+
+import { _copy, writableStreamFromWriter } from 'https://deno.land/std@0.145.0/streams/mod.ts';
+
+async function remoteCopy(src: URL, dst: URL, _options?: { bufSize?: number }) {
+	const fileResponse = await fetch(src);
+
+	if (fileResponse.body) {
+		const file = await Deno.open(ensurePath(traversal(dst)), { write: true, create: true });
+		const writableStream = writableStreamFromWriter(file);
+		await fileResponse.body.pipeTo(writableStream);
+	}
 }
