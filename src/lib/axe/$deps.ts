36359cf9cec3f788066746b35a50746b6080747c
Roy Ivy III <rivy.dev@gmail.com>
2022-06-26 10:57:28 -0500

    refactor/axe ~ ($shared) consolidate shared content into single module

--- src/lib/axe/$deps.ts
+++ src/lib/axe/$deps.ts
@@ -39,13 +39,3 @@ export * as $levels from 'https://esm.sh/rfc-log-levels@3.17.0';
 export type { Location, Offset } from 'https://esm.sh/get-current-line@6.6.0';
 export type { LevelInfo, LevelsMap as LevelMap } from 'https://esm.sh/rfc-log-levels@3.17.0';
 export type { DeepReadonly } from 'https://esm.sh/ts-essentials@9.0.0';
-
-//=== * shared
-
-// export { decode, encode } from 'https://deno.land/std@0.85.0/encoding/utf8.ts'; // 'utf8.ts' was removed via commit 5bc18f5d86
-export const decoder = new TextDecoder();
-export const decode = (input?: Uint8Array): string => decoder.decode(input);
-export const encoder = new TextEncoder();
-export const encode = (input?: string): Uint8Array => encoder.encode(input);
-
-export const inspect = Deno.inspect;
---
+++ src/lib/axe/$shared.ts
@@ -0,0 +1,7 @@
+// export { decode, encode } from 'https://deno.land/std@0.85.0/encoding/utf8.ts'; // 'utf8.ts' was removed via commit 5bc18f5d86
+export const decoder = new TextDecoder();
+export const decode = (input?: Uint8Array): string => decoder.decode(input);
+export const encoder = new TextEncoder();
+export const encode = (input?: string): Uint8Array => encoder.encode(input);
+
+export const inspect = Deno.inspect;
