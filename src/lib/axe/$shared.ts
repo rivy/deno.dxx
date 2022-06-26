e8030e08c3679360bd29c894940757a830eb1937
Roy Ivy III <rivy.dev@gmail.com>
2022-06-26 10:59:55 -0500

    refactor/axe ~ ($shared) add common default `inspect()` options

--- src/lib/axe/$shared.ts
+++ src/lib/axe/$shared.ts
@@ -1,3 +1,10 @@
+export const DEFAULT_INSPECT_OPTIONS: Deno.InspectOptions = {
+	colors: true,
+	compact: true,
+	depth: Infinity,
+	iterableLimit: Infinity,
+};
+
 // export { decode, encode } from 'https://deno.land/std@0.85.0/encoding/utf8.ts'; // 'utf8.ts' was removed via commit 5bc18f5d86
 export const decoder = new TextDecoder();
 export const decode = (input?: Uint8Array): string => decoder.decode(input);
