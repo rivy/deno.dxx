5669602ef6038c1bce9c057c1dc92394e0f62a4b
Roy Ivy III <rivy.dev@gmail.com>
2022-08-07 14:24:21 -0500

    tests ~ add diagnostic output for `deno lint` failures

--- tests/00.project.style.test.ts
+++ tests/00.project.style.test.ts
@@ -113,9 +113,14 @@ test(`style ~ \`${cSpellCmd}\` succeeds`, async () => {
 
 test('style ~ `deno lint` succeeds', async () => {
 	const p = Deno.run({ cmd: ['deno', 'lint'], stdin: 'null', stdout: 'piped', stderr: 'piped' });
-	const [status] = await Promise.all([p.status(), p.output(), p.stderrOutput()]).finally(() =>
-		p.close()
+	const [status, out, err] = await Promise.all([p.status(), p.output(), p.stderrOutput()]).finally(
+		() => p.close()
 	);
+	if (!status.success) {
+		console.warn('`deno lint` status', status);
+		console.warn(decode(out).replace(/\r?\n$/ms, ''));
+		console.warn(decode(err).replace(/\r?\n$/ms, ''));
+	}
 	assert(status.success, '`deno lint` fails');
 });
 
