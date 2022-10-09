0a3547a1ffe3131103f030fcb6880e82c6c3616f
Roy Ivy III <rivy.dev@gmail.com>
2022-10-09 12:47:24 -0500

    refactor/deps ~ update cli-specific deps

--- src/lib/$deps.cli.ts
+++ src/lib/$deps.cli.ts
@@ -2,8 +2,8 @@
 
 //==== modules
 
-export { default as $yargs } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@6d3786caa7/deno.ts';
+export { default as $yargs } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@600b4c7bd9/deno.ts'; // v17.6.0-rivy-deno
 
 //=== types
 
-export type { Arguments as YargsArguments } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@6d3786caa7/deno-types.ts';
+export type { Arguments as YargsArguments } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@600b4c7bd9/deno-types.ts'; // v17.6.0-rivy-deno
