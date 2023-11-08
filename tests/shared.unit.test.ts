92379cfa78417c7bca1f6105f4f7b32ab999bf03
Roy Ivy III <rivy.dev@gmail.com>
2023-11-07 21:19:02 -0600

    tests ~ add error stack parse testing
    - ref: [🐛 Error stack trace line numbers are incorrect](https://github.com/denoland/deno/issues/21106)

---
+++ tests/shared.unit.test.ts
@@ -0,0 +1,25 @@
+// spell-checker:ignore (fns) chdir ; (names) Deno ; (options) nullglob ; (people) Roy Ivy III * rivy
+
+import { assert } from './$deps.ts';
+import { test } from './$shared.ts';
+
+import { callersFromStackTrace } from '../src/lib/$shared.ts';
+
+// //===
+// await panicIfMissingPermits(['env', 'read']);
+// //===
+
+// review: [SO ~ (JS) determine current line number](https://stackoverflow.com/questions/2343343/how-can-i-determine-the-current-line-number-in-javascript) @@ <https://archive.is/vw4eN>
+
+test('callersFromStack', () => {
+	const [callers, calledFromLine] = [callersFromStackTrace(), 15];
+	const calledFromURL = import.meta.url;
+	console.log({ calledFromLine, calledFromURL, callers });
+	// pop off Deno implementation detail callers
+	while ((callers.length > 0) && (callers[callers.length - 1]?.startsWith('ext:'))) {
+		const _ = callers.pop();
+	}
+	console.log("removed 'ext:*'", { callers });
+	const _ = callers.pop(); // pop off `Deno.test()` caller
+	assert(callers[callers.length - 1]?.startsWith(`${calledFromURL}:${calledFromLine}`));
+});
