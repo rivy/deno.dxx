668e162c4507bcccb57a904e88cb4588112644f0
Roy Ivy III <rivy.dev@gmail.com>
2024-04-29 13:46:21 -0500

    wip/refactor ~ eg and tests - revise regex varible naming

--- src/lib/eol.ts
+++ src/lib/eol.ts
@@ -1,18 +1,18 @@
 // EOL conversion handler
 
-const newline = /\r?\n|\n/g;
+const allNewlines = /\r?\n|\r/g;
 const CR_ = '\r';
 const LF_ = '\n';
 const CRLF_ = '\r\n';
 
 export const eol = {
 	CR(s: string) {
-		return s.replace(newline, CR_);
+		return s.replace(allNewlines, CR_);
 	},
 	CRLF(s: string) {
-		return s.replace(newline, CRLF_);
+		return s.replace(allNewlines, CRLF_);
 	},
 	LF(s: string) {
-		return s.replace(newline, LF_);
+		return s.replace(allNewlines, LF_);
 	},
 };
