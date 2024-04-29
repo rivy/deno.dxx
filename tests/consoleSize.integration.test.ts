450a8cf1e5360de33f83cfb69f6f0ca7157c8896
Roy Ivy III <rivy.dev@gmail.com>
2024-04-29 17:59:09 -0500

    tests ~ add consoleSize integration tests

---
+++ tests/consoleSize.integration.test.ts
@@ -0,0 +1,81 @@
+import { /* $fs, $path, */ assert /* , assertEquals */ } from './$deps.ts';
+import {
+	decode,
+	// deepEqual,
+	// env,
+	// panicIfMissingPermits,
+	// pathToOsStyle,
+	// projectPath as maybeProjectPath,
+	test,
+} from './$shared.ts';
+
+// import * as $consoleSize from './src/lib/console-size.ts';
+
+const isWinOS = Deno.build.os == 'windows';
+
+const shell = isWinOS ? 'cmd' : 'sh';
+const shellOptions = isWinOS ? ['/d/c'] : ['-c'];
+
+test('consoleSize ~ fully redirected, no permissions', () => {
+	const process = new Deno.Command('deno', {
+		args: ['run', './tests/helpers/consoleSize.display-results.ts'],
+		stdin: 'null',
+		stdout: 'piped',
+		stderr: 'piped',
+	});
+	const { code, stdout, stderr } = process.outputSync();
+	const out = decode(stdout);
+	const err = decode(stderr);
+	if (code != 0) console.warn({ code, out, err });
+	const results = JSON.parse(out);
+	console.debug({ code, results });
+	assert(code == 0, 'consoleSize integration test succeeds');
+});
+
+test('consoleSize ~ fully redirected, run permission', () => {
+	const process = new Deno.Command('deno', {
+		args: ['run', '--allow-run', './tests/helpers/consoleSize.display-results.ts'],
+		stdin: 'null',
+		stdout: 'piped',
+		stderr: 'piped',
+	});
+	const { code, stdout, stderr } = process.outputSync();
+	const out = decode(stdout);
+	const err = decode(stderr);
+	if (code != 0) console.warn({ code, out, err });
+	const results = JSON.parse(out);
+	console.debug({ code, results });
+	assert(code == 0, 'consoleSize integration test succeeds');
+});
+
+test('consoleSize ~ fully redirected, full permissions', () => {
+	const process = new Deno.Command('deno', {
+		args: ['run', '-A', './tests/helpers/consoleSize.display-results.ts'],
+		stdin: 'null',
+		stdout: 'piped',
+		stderr: 'piped',
+	});
+	const { code, stdout, stderr } = process.outputSync();
+	const out = decode(stdout);
+	const err = decode(stderr);
+	if (code != 0) console.warn({ code, out, err });
+	const results = JSON.parse(out);
+	console.debug({ code, results });
+	assert(code == 0, 'consoleSize integration test succeeds');
+});
+
+test('consoleSize ~ fully redirected, full permissions, via shell', () => {
+	const process = new Deno.Command(shell, {
+		args: [...shellOptions, 'deno run -A ./tests/helpers/consoleSize.display-results.ts | cat'],
+		stdin: 'null',
+		stdout: 'piped',
+		stderr: 'piped',
+	});
+	const { code, stdout, stderr } = process.outputSync();
+	const out = decode(stdout);
+	const err = decode(stderr);
+	if (code != 0) console.warn({ code, out, err });
+	const results = JSON.parse(out);
+	console.debug({ code, results });
+	assert(code == 0, 'consoleSize integration test succeeds');
+});
