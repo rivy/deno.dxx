8168d15176b218fce815dd3c871c3e83b1dd5e68
Roy Ivy III <rivy.dev@gmail.com>
2024-04-29 17:59:09 -0500

    tests ~ add consoleSize integration tests

---
+++ tests/helpers/consoleSize.display-results.ts
@@ -0,0 +1,11 @@
+import * as M from '../../src/lib/consoleSize.ts';
+
+const results = await Promise.all(
+	Object.entries(M).map(async ([fn, v]) => await { fn, v: await v() }),
+);
+
+const output = JSON.stringify(results, null, 0)
+	.replace(/\[\{/g, '[\n {')
+	.replace(/,\{/g, ',\n {')
+	.replace(/\}\]/g, '}\n]');
+console.log(output);
