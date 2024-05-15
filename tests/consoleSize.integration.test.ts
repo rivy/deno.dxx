13b2865a79d2d3e716e09634fe01452d68084146
Roy Ivy III <rivy.dev@gmail.com>
2024-05-15 16:23:37 -0500

    tests ~ add more in-depth consoleSize integration tests

--- tests/consoleSize.integration.test.ts
+++ tests/consoleSize.integration.test.ts
@@ -1,7 +1,7 @@
 import { $semver, /* $fs, $path, */ assert /* , assertEquals */ } from './$deps.ts';
 import {
 	decode,
-	// deepEqual,
+	deepEqual,
 	// env,
 	// panicIfMissingPermits,
 	// pathToOsStyle,
@@ -21,116 +21,194 @@ const denoOptionsFFI = $semver.satisfies(Deno.version.deno, '>= 1.38.0')
 	: ['--allow-ffi', '--unstable'];
 
 test('consoleSize ~ fully redirected, no permissions', () => {
-	const process = new Deno.Command('deno', {
-		args: ['run', './tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = 'deno';
+	const args = ['run', './tests/helpers/consoleSize.display-results.ts'];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, run permission', () => {
-	const process = new Deno.Command('deno', {
-		args: ['run', '--allow-run', './tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = 'deno';
+	const args = ['run', '--allow-run', './tests/helpers/consoleSize.display-results.ts'];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, FFI permission', () => {
-	const process = new Deno.Command('deno', {
-		args: ['run', '-A', ...denoOptionsFFI, './tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = 'deno';
+	const args = [
+		'run',
+		'--allow-all',
+		...denoOptionsFFI,
+		'./tests/helpers/consoleSize.display-results.ts',
+	];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, full permissions', () => {
-	const process = new Deno.Command('deno', {
-		args: ['run', '-A', './tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = 'deno';
+	const args = ['run', '--allow-all', './tests/helpers/consoleSize.display-results.ts'];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, full permissions, via shell', () => {
-	const process = new Deno.Command(shell, {
-		args: [...shellOptions, 'deno run -A ./tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = shell;
+	const args = [
+		...shellOptions,
+		'deno run --allow-all ./tests/helpers/consoleSize.display-results.ts',
+	];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, full permissions + FFI', () => {
-	const process = new Deno.Command('deno', {
-		args: ['run', '-A', ...denoOptionsFFI, './tests/helpers/consoleSize.display-results.ts'],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = 'deno';
+	const args = [
+		'run',
+		'--allow-all',
+		...denoOptionsFFI,
+		'./tests/helpers/consoleSize.display-results.ts',
+	];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
 
 test('consoleSize ~ fully redirected, full permissions + FFI, via shell', () => {
-	const process = new Deno.Command(shell, {
-		args: [
-			...shellOptions,
-			`deno run -A ${denoOptionsFFI.join(' ')} ./tests/helpers/consoleSize.display-results.ts`,
-		],
-		stdin: 'null',
-		stdout: 'piped',
-		stderr: 'piped',
-	});
+	const cmd = shell;
+	const args = [
+		...shellOptions,
+		`deno run --allow-all ${denoOptionsFFI.join(
+			' ',
+		)} ./tests/helpers/consoleSize.display-results.ts`,
+	];
+	const process = new Deno.Command(cmd, { args, stdin: 'null', stdout: 'piped', stderr: 'piped' });
 	const { code, stdout, stderr } = process.outputSync();
 	const out = decode(stdout);
 	const err = decode(stderr);
 	if (code != 0) console.warn({ code, out, err });
-	const results = JSON.parse(out);
-	console.log({ code, results });
+	const results = JSON.parse(out) as { fn: string; v: { columns: number; rows: number } }[];
+	console.log({ cmd, args, code, results });
 	assert(code == 0, 'consoleSize integration test succeeds');
+	const consoleSizeResult = results[0];
+	assert(consoleSizeResult.fn === 'consoleSize', 'consoleSize() is first result');
+	assert(
+		results.reduce((matchOrNull, result) => {
+			const equalOrNull = result.v == null || deepEqual(consoleSizeResult.v, result.v);
+			if (!equalOrNull) {
+				console.warn('Unmatched consoleSize sub-result\n', { consoleSizeResult, result });
+			}
+			return matchOrNull && equalOrNull;
+		}, true),
+		'all results match (or are null)',
+	);
 });
