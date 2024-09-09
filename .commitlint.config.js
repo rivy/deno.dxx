add1ac4c9f2b6e052aae76799cacb923499beda3
Roy Ivy III <rivy.dev@gmail.com>
2024-09-08 19:19:36 -0500

    maint/dev ~ fix `deno lint` complaint (no-node-globals)

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -7,6 +7,8 @@
 /* @prettier */
 // note: (dprint) {.dprint.json}.prettier.associations should contain the name of this file
 
+// deno-lint-ignore-file no-node-globals -- allow use of NodeJS global `process` (CommitLint expects NodeJS-flavor JS)
+
 const isNPMTestDist = !!process.env.npm_config_test_dist;
 const isTestDist = !!process.env.test_dist;
 const isTestRelease = !!process.env.test_release;
