8ed1ee4243a0095f1ed67ee380d855b71cb21292
Roy Ivy III <rivy.dev@gmail.com>
2025-01-11 10:52:30 -0600

    fix ~ yargs/y18n failed write to read-only 'code' field in sealed Error

--- src/lib/$deps.cli.ts
+++ src/lib/$deps.cli.ts
@@ -13,8 +13,8 @@
 // ```
 // * revise URL to use 'esm.sh' instead of 'cdn.jsdelivr.net' for compatibility with Deno-2.0 "import" permission defaults
 // export { default as $yargs } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@6be59a7fda/deno.ts'; // v17.7.2-deno-rivy
-// export { default as $yargs } from 'https://esm.sh/gh/rivy-js/yargs@f4b8034a75/deno.ts?pin=v135'; // v17.7.2-deno-rivy
-export { default as $yargs } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@f4b8034a75/deno.ts'; // v17.7.2-deno-rivy
+// export { default as $yargs } from 'https://esm.sh/gh/rivy-js/yargs@18d34da010/deno.ts?pin=v135'; // v17.7.2-deno-rivy+1 // Deno-v2.1.5+ fixes
+export { default as $yargs } from 'https://cdn.jsdelivr.net/gh/rivy-js/yargs@18d34da010/deno.ts'; // v17.7.2-deno-rivy+1 // Deno-v2.1.5+ fixes
 
 //==== types
 
