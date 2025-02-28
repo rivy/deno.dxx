0a6d5db46737179b96f7d6e8f27bb193ff15d5ee
Roy Ivy III <rivy.dev@gmail.com>
2025-02-27 20:55:30 -0600

    fix/vendor ~ correct typing case of `extglobChars()`

--- vendor/@types/picomatch@2.2.2/constants.d.ts
+++ vendor/@types/picomatch@2.2.2/constants.d.ts
@@ -126,7 +126,7 @@ declare const constants: {
 
     SEP: string;
 
-    extGlobChars(chars: { STAR: string }): Record<string, { type: string; open: string; close: string }>;
+    extglobChars(chars: { STAR: string }): Record<string, { type: string; open: string; close: string }>;
 
     globChars<T extends boolean>(win32: T): T extends true ? typeof WINDOWS_CHARS : typeof POSIX_CHARS;
 };
