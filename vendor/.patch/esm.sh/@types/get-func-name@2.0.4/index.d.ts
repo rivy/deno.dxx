094e5274fc8edad50d781181669f312b008f46cc
Roy Ivy III <rivy.dev@gmail.com>
2025-07-28 18:17:38 -0500

    maint/deps ~ update vendor typings patches (fix for Deno-v2+)

---
+++ vendor/.patch/esm.sh/@types/get-func-name@2.0.4/index.d.ts
@@ -0,0 +1,2 @@
+export default getFuncName;
+declare function getFuncName(fn: Function): string; // eslint-disable-line @typescript-eslint/no-unsafe-function-type
---
+++ vendor/.patch/esm.sh/assertion-error@1.1.0/index.d.ts
@@ -0,0 +1,8 @@
+type AssertionError<T = {}> = Error & T & {
+    showDiff: boolean;
+};
+interface AssertionErrorConstructor {
+    new <T = {}>(message: string, props?: T, ssf?: Function): AssertionError<T>;
+}
+declare const AssertionError: AssertionErrorConstructor;
+export default AssertionError;
