494435bc3505b45ec507494993c185bb9ae39280
Roy Ivy III <rivy.dev@gmail.com>
2025-08-23 18:29:50 -0500

    tests ~ add/revise `pathIntoURL()` tests

--- tests/shared.unit.test.ts
+++ tests/shared.unit.test.ts
@@ -24,7 +24,7 @@ test('callersFromStack', () => {
 	assert(callers[callers.length - 1]?.startsWith(`${calledFromURL}:${calledFromLine}`));
 });
 
-// intoURL
+// pathIntoURL()
 
 test('pathIntoURL: valid file path', () => {
 	const path = 'C:\\Users\\Morpheus\\file.ext';
@@ -39,8 +39,8 @@ test('pathIntoURL: valid file path', () => {
 });
 
 test('pathIntoURL: relative path with base', () => {
-	const base = new URL('file:///C:/Users/Morpheus/');
 	const path = 'dir/file.ext';
+	const base = new URL('file:///C:/Users/Morpheus/');
 	const expected = 'file:///C:/Users/Morpheus/dir/file.ext';
 	console.log({ base, path, expected });
 	const result = pathIntoURL(path, { base });
@@ -67,7 +67,10 @@ test('pathIntoURL: absolute POSIX path', () => {
 
 test('pathIntoURL: opaque URL', () => {
 	const path = 'opaque:path';
+	const expected_origin = 'null';
 	const expected_protocol = 'opaque:';
+	const expected_host = '';
+	const expected_hostname = '';
 	const expected_pathname = 'path';
 	const expected_href = `${expected_protocol}${expected_pathname}`;
 	console.log({ path, expected_href });
@@ -76,43 +79,53 @@ test('pathIntoURL: opaque URL', () => {
 	// assertEquals(result, undefined, 'Result should be undefined for invalid input');
 	assert(result instanceof URL, 'Result should be a URL');
 	assertEquals(result.href, expected_href, 'URL should resolve correctly');
+	assertEquals(result.origin, expected_origin, '`origin` should be match');
 	assertEquals(result.protocol, expected_protocol, '`protocol` should be match');
+	assertEquals(result.host, expected_host, '`host` should match');
+	assertEquals(result.hostname, expected_hostname, '`hostname` should match');
 	assertEquals(result.pathname, expected_pathname, '`pathname` should match');
 });
 
-// // test('pathIntoURL: URL input', () => {
-// // 	const input = new URL('https://example.com/resource');
-// // 	const result = pathIntoURL(input);
-// // 	assert(result instanceof URL, 'Result should be a URL');
-// // 	assertEquals(result.href, input.href, 'URL should match the input');
-// // });
+test('pathIntoURL: string URL (HTTPS) input', () => {
+	const input = new URL('https://example.com/resource');
+	const result = pathIntoURL(input.href);
+	assert(result instanceof URL, 'Result should be a URL');
+	assertEquals(result.href, input.href, 'URL should match the input');
+});
 
-// test('pathIntoURL: drive relative path', () => {
-// 	const path = 'C:file.ext';
-// 	console.log({ path });
-// 	// const result_forPOSIX = pathIntoURL(path, { forPlatform: 'POSIX' });
-// 	// const result_forWinOS = pathIntoURL(path, { forPlatform: 'WinOS' });
-// 	const result = pathIntoURL(path);
-// 	// console.log({ result_forPOSIX, result_forWinOS, result });
-// 	assert(result instanceof URL, 'Result should be a URL');
-// 	assertEquals(result.protocol, 'file:', 'Protocol should be file:');
-// 	assert(
-// 		result.pathname.startsWith('/C:/'),
-// 		'Pathname should be absolute with initial drive letter',
-// 	);
-// 	assert(result.pathname.includes('/file.ext'), 'Pathname should include file portion');
-// 	// assertEquals(result, result_forWinOS, 'Results should match for WinOS option');
-// });
+test('pathIntoURL: drive relative path', () => {
+	const path = 'C:path/file.ext';
+	console.log({ path });
+	const result_forPOSIX = pathIntoURL(path, { forPlatform: 'POSIX' });
+	const result_forWinOS = pathIntoURL(path, { forPlatform: 'WinOS' });
+	const result = pathIntoURL(path);
+	console.log({ result_forPOSIX, result_forWinOS, result });
+	assert(result instanceof URL, 'Result should be a URL');
+	assertEquals(result.protocol, 'file:', '`protocol` should be "file:"');
+	assert(
+		result.pathname.startsWith('/C:/'),
+		'`pathname` should be absolute with initial drive letter',
+	);
+	assert(
+		result.pathname.endsWith('/path/file.ext'),
+		'`pathname` should end with path/file portion',
+	);
+	// assertEquals(result, result_forWinOS, 'Results should match for WinOS option');
+});
+
+// pathIntoURL()
+// * verify file system path semantics are always used
 
 test('pathIntoURL: file system (absolute) path semantics with base file', () => {
-	const base_path = 'file://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = '/bar_path/bar_file.ext';
+	const base_path = 'file://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'file:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -127,14 +140,15 @@ test('pathIntoURL: file system (absolute) path semantics with base file', () => 
 });
 
 test('pathIntoURL: file system (relative) path semantics with base file', () => {
-	const base_path = 'file://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = 'bar_path/bar_file.ext';
+	const base_path = 'file://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'file:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/foo_path/foo_file_or_folder/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -149,14 +163,15 @@ test('pathIntoURL: file system (relative) path semantics with base file', () => 
 });
 
 test('pathIntoURL: file system (absolute) path semantics with base http', () => {
-	const base_path = 'http://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = '/bar_path/bar_file.ext';
+	const base_path = 'http://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'http:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -171,14 +186,15 @@ test('pathIntoURL: file system (absolute) path semantics with base http', () => 
 });
 
 test('pathIntoURL: file system (relative) path semantics with base http', () => {
-	const base_path = 'http://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = 'bar_path/bar_file.ext';
+	const base_path = 'http://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'http:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/foo_path/foo_file_or_folder/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -193,14 +209,15 @@ test('pathIntoURL: file system (relative) path semantics with base http', () => 
 });
 
 test('pathIntoURL: file system (absolute) path semantics with base foo-scheme', () => {
-	const base_path = 'foo-scheme://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = '/bar_path/bar_file.ext';
+	const base_path = 'foo-scheme://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'foo-scheme:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -215,14 +232,15 @@ test('pathIntoURL: file system (absolute) path semantics with base foo-scheme', 
 });
 
 test('pathIntoURL: file system (relative) path semantics with base foo-scheme', () => {
-	const base_path = 'foo-scheme://domain/foo_path/foo_file_or_folder';
-	const base_as_file = new URL(base_path);
-	const base_as_folder = new URL(base_path + $path.posix.sep);
 	const path = 'bar_path/bar_file.ext';
+	const base_path = 'foo-scheme://domain/foo_path/foo_file_or_folder';
 	const expected_protocol = 'foo-scheme:';
 	const expected_hostname = 'domain';
 	const expected_pathname = '/foo_path/foo_file_or_folder/bar_path/bar_file.ext';
 	const expected_href = `${expected_protocol}//${expected_hostname}${expected_pathname}`;
+	//
+	const base_as_file = new URL(base_path);
+	const base_as_folder = new URL(base_path + $path.posix.sep);
 	console.log({ base_as_file, base_as_folder, path, expected_href });
 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
@@ -235,3 +253,64 @@ test('pathIntoURL: file system (relative) path semantics with base foo-scheme', 
 	assertEquals(result.pathname, expected_pathname, '`pathname` should match');
 	assertEquals(result.href, expected_href, 'URL should resolve correctly');
 });
+
+// FixME: [2025-08-23; rivy] define and test various cases with URLs containing hash and search portions
+// ! * `file:` paths should probably not have either, but as a special case, allowing construction of URLs with search and hash portions
+// ! * per AI, only the origin and path from the base are considered for resolution; username, password, query, and hash are always dropped
+
+// FixME: [2025-08-23; rivy] define and test various cases with Opaque base URLs
+// * per AI...
+// new URL('x', new URL('mailto:foo@bar.com')).href;        // "mailto:x"
+// new URL('y/z', new URL('mailto:foo@bar.com')).href;      // "mailto:y/z"
+// new URL('./rel', new URL('mailto:foo@bar.com')).href;    // "mailto:rel"
+// new URL('/abs', new URL('mailto:foo@bar.com')).href;     // probably should be undefined (or an error), not "mailto:/abs", as the alternative of forcing 'mailto:foo@bar.com' into a hierarchical form with authority would seem to violate the spec and expectation
+
+// test('pathIntoURL: file system ("absolute") path semantics with base opaque scheme', () => {
+// 	const path = '/bar_path@bar_file.ext';
+// 	const base_path = 'mailto:foo@bar.domain';
+// 	const expected_origin = 'null';
+// 	const expected_protocol = 'mailto:';
+// 	const expected_hostname = '';
+// 	const expected_pathname = '/bar_path@bar_file.ext';
+// 	const expected_href = `${expected_protocol}${expected_pathname}`;
+// 	//
+// 	const base_as_file = new URL(base_path);
+// 	const base_as_folder = new URL(base_path + $path.posix.sep);
+// 	console.log({ base_as_file, base_as_folder, path, expected_href });
+// 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
+// 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
+// 	console.log({ result_from_base_file, result_from_base_folder });
+// 	assertEquals(result_from_base_file, result_from_base_file, 'Variation results should be equal');
+// 	const result = result_from_base_file;
+// 	assert(result instanceof URL, 'Result should be a URL');
+// 	assertEquals(result.origin, expected_origin, '`origin` should match');
+// 	assertEquals(result.protocol, expected_protocol, '`protocol` should match');
+// 	assertEquals(result.hostname, expected_hostname, '`hostname` should match');
+// 	assertEquals(result.pathname, expected_pathname, '`pathname` should match');
+// 	assertEquals(result.href, expected_href, 'URL should resolve correctly');
+// });
+
+// test('pathIntoURL: file system (relative) path semantics with base opaque scheme', () => {
+// 	const path = 'bar_path/bar_file.ext';
+// 	const base_path = 'mailto:foo@bar.domain';
+// 	const expected_origin = 'null';
+// 	const expected_protocol = 'mailto:';
+// 	const expected_hostname = '';
+// 	const expected_pathname = 'bar_path/bar_file.ext';
+// 	const expected_href = `${expected_protocol}${expected_pathname}`;
+// 	//
+// 	const base_as_file = new URL(base_path);
+// 	const base_as_folder = new URL(base_path + $path.posix.sep);
+// 	console.log({ base_as_file, base_as_folder, path, expected_href });
+// 	const result_from_base_file = pathIntoURL(path, { base: base_as_file });
+// 	const result_from_base_folder = pathIntoURL(path, { base: base_as_folder });
+// 	console.log({ result_from_base_file, result_from_base_folder });
+// 	assertEquals(result_from_base_file, result_from_base_file, 'Variation results should be equal');
+// 	const result = result_from_base_file;
+// 	assert(result instanceof URL, 'Result should be a URL');
+// 	assertEquals(result.origin, expected_origin, '`origin` should match');
+// 	assertEquals(result.protocol, expected_protocol, '`protocol` should match');
+// 	assertEquals(result.hostname, expected_hostname, '`hostname` should match');
+// 	assertEquals(result.pathname, expected_pathname, '`pathname` should match');
+// 	assertEquals(result.href, expected_href, 'URL should resolve correctly');
+// });
