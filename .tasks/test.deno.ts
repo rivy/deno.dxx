e302b59036c5766cf1d50c5de4c4cc3dd44dd6e5
Roy Ivy III <rivy.dev@gmail.com>
2026-08-11 11:40:09 -0500

    maint/dev ~ (WIP) add deno tasks to overcome v2.9.4 `deno test ...` test display changes
    ## why
    
    Deno-v2.9.4 added filtering to test names changing all control characters (including ANSI
    control sequences) to visible escape sequences. This is for robustness and security to
    avoid allowing display of test names to corrupt the display with aggressive sequences.
    
    Post test output filtering can restore the intended color choices.
    
    A longer term solution might be revising the name to be completely visible ANSI characters
    with signal sequences that can be replaced with color, ie, `[file:line] name ... <...>`
    would be replaced with `<dim>file:line<UNdim> <bold>name<UNbold> ... <...>`.

---
+++ .tasks/test.deno.ts
@@ -0,0 +1,23 @@
+const child = new Deno.Command(Deno.execPath(), {
+	args: ['test', '-A', ...Deno.args],
+	cwd: new URL('..', import.meta.url),
+	stdout: 'piped',
+	stderr: 'inherit',
+}).spawn();
+
+await child.stdout
+	.pipeThrough(new TextDecoderStream())
+	.pipeThrough(
+		// reverse the ANSI color escapes
+		// ## why: Deno-v2.9.4 sanitizes test names, replacing all control characters (and ANSI escapes) with escaped versions
+		// * additionally TAP output has escaped ANSI sequences starting with `\\x1b` instead of just `\x1b`
+		new TransformStream<string, string>({
+			transform(s, controller) {
+				controller.enqueue(s.replace(/\\?\\x1b(\[[0-9;]*m)/g, '\x1b$1'));
+			},
+		}),
+	)
+	.pipeThrough(new TextEncoderStream())
+	.pipeTo(Deno.stdout.writable, { preventClose: true });
+
+Deno.exit((await child.status).code);
