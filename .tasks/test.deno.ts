a0327fe8dd2a5d366a1d8b85c23a2a2a51e79dd9
Roy Ivy III <rivy.dev@gmail.com>
2026-08-12 20:35:06 -0500

    maint/dev ~ WIP update task scripts

--- .tasks/test.deno.ts
+++ .tasks/test.deno.ts
@@ -1,3 +1,5 @@
+// import * as $colors from 'https://deno.land/std@0.134.0/fmt/colors.ts';
+
 const child = new Deno.Command(Deno.execPath(), {
 	args: ['test', '-A', ...Deno.args],
 	cwd: new URL('..', import.meta.url),
@@ -8,7 +10,7 @@ const child = new Deno.Command(Deno.execPath(), {
 await child.stdout
 	.pipeThrough(new TextDecoderStream())
 	.pipeThrough(
-		// reverse the ANSI color escapes
+		// reverse any ANSI color escapes (from Deno-v2.9.4+)
 		// ## why: Deno-v2.9.4 sanitizes test names, replacing all control characters (and ANSI escapes) with escaped versions
 		// * additionally TAP output has escaped ANSI sequences starting with `\\x1b` instead of just `\x1b`
 		new TransformStream<string, string>({
@@ -17,6 +19,20 @@ await child.stdout
 			},
 		}),
 	)
+	// .pipeThrough(
+	// 	// add ANSI color to descriptions (for pure ASCII test descriptions/names)
+	// 	new TransformStream<string, string>({
+	// 		transform(s, controller) {
+	// 			controller.enqueue(
+	// 				s.replace(
+	// 					/^(\S+?:\d+)\s(::)\s(.*)$/g,
+	// 					(_match, location, _separator, /* description, */ result) =>
+	// 						`${$colors.dim(location)} ${result}`,
+	// 				),
+	// 			);
+	// 		},
+	// 	}),
+	// )
 	.pipeThrough(new TextEncoderStream())
 	.pipeTo(Deno.stdout.writable, { preventClose: true });
 
